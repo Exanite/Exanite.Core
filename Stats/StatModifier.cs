66ea9000a8b7a23369e6ad67990cb88f05ce9c87
Exanite <Exanite78012@gmail.com>
2018-11-18 15:59:15 -0800

    StatModifier.cs: Separated out from StatHolder.cs

---
+++ Stats/StatModifier.cs
@@ -0,0 +1,16 @@
+﻿namespace Exanite.Stats
+{
+	public class StatModifier
+	{
+		public readonly float Value;
+		public readonly StatModifierType Type;
+		public readonly object Source;
+
+		public StatModifier(float value, StatModifierType type, object source)
+		{
+			Value = value;
+			Type = type;
+			Source = source;
+		}
+	}
+}
