f62e754b16b344ce62ecbde55cca6109bf558f09
Exanite <Exanite78012@gmail.com>
2018-11-25 02:06:30 -0800

    TrackedStat.cs: Added Fields and Properties

---
+++ StatSystem/TrackedStat.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exanite.StatSystem
+{
+	public class TrackedStat
+	{
+		#region Fields and Properties
+
+		protected float baseValue;
+		protected float flatValue;
+		protected float incValue;
+		protected float multValue;
+		protected float finalValue;
+
+		public float BaseValue
+		{
+			get
+			{
+				return baseValue;
+			}
+
+			protected set
+			{
+				baseValue = value;
+			}
+		}
+		public float FlatValue
+		{
+			get
+			{
+				return flatValue;
+			}
+
+			protected set
+			{
+				flatValue = value;
+			}
+		}
+		public float IncValue
+		{
+			get
+			{
+				return incValue;
+			}
+
+			protected set
+			{
+				incValue = value;
+			}
+		}
+		public float MultValue
+		{
+			get
+			{
+				return multValue;
+			}
+
+			protected set
+			{
+				multValue = value;
+			}
+		}
+		public float FinalValue
+		{
+			get
+			{
+				return finalValue;
+			}
+
+			protected set
+			{
+				finalValue = value;
+			}
+		}
+
+		#endregion
+	}
+}
