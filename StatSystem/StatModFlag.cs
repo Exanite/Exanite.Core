b9d8c35645e5d9c434678a7242b0fbadf729da8b
Exanite <Exanite78012@gmail.com>
2018-11-24 22:36:08 -0800

    StatModFlag.cs: Added extra flags

--- StatSystem/StatModFlag.cs
+++ StatSystem/StatModFlag.cs
@@ -2,18 +2,45 @@
 {
 	public enum StatModFlag
 	{
+		Damage,
+
+		Attack,
+		Spell,
+		Hit,
+		DamageOverTime,
+
+		Unarmed,
+		Dagger,
+		Axe,
+		Sword,
+		Mace,
+		Spear,
+		Bow,
+		Crossbow,
+		Staff,
+		Wand,
+
 		Physical,
 		Lightning,
 		Ice,
 		Fire,
 		Chaos,
+		Elemental,
 
 		Melee,
+		Ranged,
+		OneHand,
+		TwoHand,
+		DualWield,
+
 		Projectile,
-		Spell,
 		AreaOfEffect,
-		Attack,
-		Hit,
-		DamageOverTime,
+
+		Health,
+		Mana,
+		Shield,
+
+		Max,
+		Regen,
 	}
 }
