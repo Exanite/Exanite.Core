0102f81617f49906a1108d44826cc83bf0866a41
Exanite <Exanite78012@gmail.com>
2018-12-06 21:28:06 -0800

    ExaniteCore: Added some useful extensions

---
+++ Extensions/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exanite.Extensions
+{
+	public static class Extensions
+	{
+		public static bool IsNullOrEmpty(this Array array)
+		{
+			return (array == null || array.Length == 0);
+		}
+	}
+}
