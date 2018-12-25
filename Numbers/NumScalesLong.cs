42e338a605505e572cc40926ae4385529f1cbaac
Exanite <Exanite78012@gmail.com>
2018-12-24 16:24:25 -0800

    Numbers: Added NumScalesShort, NumScalesLong, and NumDisplayFormat Enums

---
+++ Numbers/NumScalesLong.cs
@@ -0,0 +1,69 @@
+﻿namespace Exanite.Numbers
+{
+	/// <summary>
+	/// Whole number names
+	/// </summary>
+	[System.Serializable]
+	public enum NumScalesLong
+	{
+		// Scales found at https://en.wikipedia.org/wiki/Names_of_large_numbers
+
+		None					=	0,
+		Thousand				=	1,
+		Million					=	2,
+		Billion					=	3,
+		Trillion				=	4,
+		Quadrillion				=	5,
+		Quintillion				=	6,
+		Sextillion				=	7,
+		Septillion				=	8,
+		Octillion				=	9,
+		Nonillion				=	10,
+
+		Decillion				=	11,
+		Undecillion				=	12,
+		Duodecillion			=	13,
+		Tredecillion			=	14,
+		Quattuordecillion		=	15,
+		Quinquadecillion		=	16,
+		Sexdecillion			=	17,
+		Septendecillion			=	18,
+		Octodecillion			=	19,
+		Novemdecillion			=	20,
+
+		Vigintillion			=	21,
+		Unvigintillion			=	22,
+		Duovigintillion			=	23,
+		Tresvigintillion		=	24,
+		Quattuorvigintillion	=	25,
+		Quinquavigintillion		=	26,
+		Sesvigintillion			=	27,
+		Septemvigintillion		=	28,
+		Octovigintillion		=	29,
+		Novemvigintillion		=	30,
+
+		Trigintillion			=	31,
+		Untrigintillion			=	32,
+		Duotrigintillion		=	33,
+		Trestrigintillion		=	34,
+		Quattuortrigintillion	=	35,
+		Quinquatrigintillion	=	36,
+		Sestrigintillion		=	37,
+		Septentrigintillion		=	38,
+		Octotrigintillion		=	39,
+		Noventrigintillion		=	40,
+
+		Quadragintillion		=	41,
+		Unquadragintillion		=	42,
+		Duoquadragintillion		=	43,
+		Tresquadragintillion	=	44,
+		Quattorquadragintillion	=	45,
+		Quinquaquadragintillion	=	46,
+		Sesquadragintillion		=	47,
+		Septenquadragintillion	=	48,
+		Octoquadragintillion	=	49,
+		Novemquadragintillion	=	50,
+
+		Quinquagintillion		=	51,
+	}
+}
---
+++ Numbers/NumScalesShort.cs
@@ -0,0 +1,78 @@
+﻿namespace Exanite.Numbers
+{
+	/// <summary>
+	/// Abbrieviations of number names
+	/// </summary>
+	[System.Serializable]
+	public enum NumScalesShort
+	{
+		// Scales found at https://en.wikipedia.org/wiki/Names_of_large_numbers
+
+		None	=	0,
+		K		=	1,
+		M		=	2,
+		B		=	3,
+		T		=	4,
+		Qd		=	5,
+		Qn		=	6,
+		Sx		=	7,
+		Sp		=	8,
+		O		=	9,
+		N		=	10,
+
+		DE		=	11,
+		UD		=	12,
+		DD		=	13,
+		TD		=	14,
+		QdD		=	15,
+		QnD		=	16,
+		SxD		=	17,
+		SpD		=	18,
+		OcD		=	19,
+		NvD		=	20,
+
+		VGN		=	21,
+		UV		=	22,
+		DV		=	23,
+		TV		=	24,
+		QdV		=	25,
+		QnV		=	26,
+		SxV		=	27,
+		SpV		=	28,
+		OcV		=	29,
+		NvV		=	30,
+
+		TGN		=	31,
+		UT		=	32,
+		DT		=	33,
+		TT		=	34,
+		QdT		=	35,
+		QnT		=	36,
+		SxT		=	37,
+		SpT		=	38,
+		OcT		=	39,
+		NvT		=	40,
+
+		QDN		=	41,
+		UQd		=	42,
+		DQd		=	43,
+		TQd		=	44,
+		QdQd	=	45,
+		QnQd	=	46,
+		SxQd	=	47,
+		SpQd	=	48,
+		OcQd	=	49,
+		NvQd	=	50,
+
+		QNN		=	51,
+		UQn		=	52,
+		DQn		=	53,
+		TQn		=	54,
+		QdQn	=	55,
+		QnQn	=	56,
+		SxQn	=	57,
+		SpQn	=	58,
+		OcQn	=	59,
+		NvQn	=	60,
+	}
+}
