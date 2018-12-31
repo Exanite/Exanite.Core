5b3f252b70335e83c0a3dc5f6980ab5d1536b929
Exanite <Exanite78012@gmail.com>
2018-12-30 23:50:41 -0800

    LargeNumber.cs: Fixed a minor typo in the summary of the class.

--- Numbers/LargeNumber.cs
+++ Numbers/LargeNumber.cs
@@ -5,7 +5,7 @@ using Exanite.Utility;
 namespace Exanite.Numbers
 {
 	/// <summary>
-	/// Used to store very large numbers (up to 999.999999x10^2^63) <para/>
+	/// Used to store very large numbers (up to 999.999999x(10^(3*2^63))) <para/>
 	/// Actual value = <see cref="Value"/> * (10 ^ (<see cref="Multiplier"/> * 3))
 	/// </summary>
 	public struct LargeNumber
