9f519e49d9789eb0dbb6b411d9db4b07ca67a0e9
Exanite <Exanite78012@gmail.com>
2019-01-29 22:12:33 -0800

    ExaniteCore: Added Math folder and MathE.cs, MathE.cs holds static methods that can be used for manipulating numbers

---
+++ Math/MathE.cs
@@ -0,0 +1,104 @@
+﻿namespace Exanite
+{
+    /// <summary>
+    /// Holds Exanite's math methods
+    /// </summary>
+    public static class MathE
+    {
+        #region Clamp
+
+        /// <summary>
+        /// Clamps a value between min and max values
+        /// </summary>
+        /// <param name="value">Value to clamp</param>
+        /// <param name="min">Minimum value</param>
+        /// <param name="max">Maximum value</param>
+        /// <returns>Clamped value</returns>
+        public static float Clamp(float value, float min, float max)
+        {
+            if (value < min)
+            {
+                return min;
+            }
+            else if (value > max)
+            {
+                return max;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        /// <summary>
+        /// Clamps a value between min and max values
+        /// </summary>
+        /// <param name="value">Value to clamp</param>
+        /// <param name="min">Minimum value</param>
+        /// <param name="max">Maximum value</param>
+        /// <returns>Clamped value</returns>
+        public static int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+            {
+                return min;
+            }
+            else if (value > max)
+            {
+                return max;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        /// <summary>
+        /// Clamps a value between min and max values
+        /// </summary>
+        /// <param name="value">Value to clamp</param>
+        /// <param name="min">Minimum value</param>
+        /// <param name="max">Maximum value</param>
+        /// <returns>Clamped value</returns>
+        public static double Clamp(double value, double min, double max)
+        {
+            if (value < min)
+            {
+                return min;
+            }
+            else if (value > max)
+            {
+                return max;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        /// <summary>
+        /// Clamps a value between min and max values
+        /// </summary>
+        /// <param name="value">Value to clamp</param>
+        /// <param name="min">Minimum value</param>
+        /// <param name="max">Maximum value</param>
+        /// <returns>Clamped value</returns>
+        public static long Clamp(long value, long min, long max)
+        {
+            if (value < min)
+            {
+                return min;
+            }
+            else if (value > max)
+            {
+                return max;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        #endregion
+    }
+}
