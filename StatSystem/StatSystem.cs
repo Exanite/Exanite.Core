a72585073b60f511d0dd856ada6b1c8a22569e4c
Exanite <Exanite78012@gmail.com>
2019-02-05 14:28:46 -0800

    StatSystem.cs: Redesigned many parts of the StatSystem

--- StatSystem/StatSystem.cs
+++ StatSystem/StatSystem.cs
@@ -1,110 +1,80 @@
-﻿using System;
+﻿using Exanite.Flags;
+using Exanite.Utility;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
-using Sirenix.Serialization;
-#if ODIN_INSPECTOR
-using Sirenix.OdinInspector;
-#endif
-using Exanite.Flags;
+using System.Linq;
 
 namespace Exanite.StatSystem
 {
     /// <summary>
-    /// Handles everything you need for a stat system in a game
+    /// Handles everything you need for a stat system
     /// </summary>
     [Serializable]
     public class StatSystem<T> where T : struct, IComparable, IConvertible, IFormattable
     {
-        #region Fields and Events
+        #region Fields and Properties
 
+        /// <summary>
+        /// Modifiers of this <see cref="StatSystem{T}"/>
+        /// </summary>
         [OdinSerialize]
-#if ODIN_INSPECTOR
         [ReadOnly]
         [TabGroup("Modifiers")]
-#endif
-        protected List<StatMod<T>> modifiers;
+        public readonly List<StatMod<T>> Modifiers;
 
         [OdinSerialize]
-#if ODIN_INSPECTOR
         [ReadOnly]
         [TabGroup("Tracked Stats")]
         [DictionaryDrawerSettings(DisplayMode = DictionaryDisplayOptions.ExpandedFoldout)]
-#endif
         protected Dictionary<string, TrackedStat<T>> trackedStats;
 
         protected static LongFlag<T> flagCache = new LongFlag<T>();
 
         /// <summary>
-        /// Delegate used for mod events
-        /// </summary>
-        /// <param name="mod">Mod involved in the event</param>
-        public delegate void ModEvent(StatMod<T> mod);
-        /// <summary>
         /// Called when a mod is added
         /// </summary>
-        [HideInInspector] public ModEvent ModAdded;
+        [HideInInspector] public Action<StatMod<T>> ModAdded;
         /// <summary>
         /// Called when a mod is removed
         /// </summary>
-        [HideInInspector] public ModEvent ModRemoved;
+        [HideInInspector] public Action<StatMod<T>> ModRemoved;
 
-        protected bool? isEnum = null;
+        protected static EnumData<T> enumData;
 
         #endregion
 
         #region Constructor
 
         /// <summary>
-        /// Creates a new StatSystem
+        /// Creates a <see langword="new"/> <see cref="StatSystem{T}"/>
         /// </summary>
         public StatSystem()
         {
-            switch (isEnum)
-            {
-                case (null):
-                    isEnum = typeof(T).IsEnum;
-                    if (isEnum == false)
-                    {
-                        throw new ArgumentException(string.Format("{0} is not an Enum Type", typeof(T)));
-                    }
-                    break;
-                case (true):
-                    break;
-                case (false):
-                    throw new ArgumentException(string.Format("{0} is not an Enum Type", typeof(T)));
-            }
+            if (enumData == null) enumData = EnumData<T>.Instance;
 
-            modifiers = new List<StatMod<T>>();
+            Modifiers = new List<StatMod<T>>();
             trackedStats = new Dictionary<string, TrackedStat<T>>();
         }
 
         #endregion
 
-        #region Retrieving Mods
-
-        public virtual List<StatMod<T>> GetAllModifiers()
-        {
-            return modifiers;
-        }
-
-        #endregion
-
-        #region Adding/Removing Mods
-
-        #region Adding Mods
+        #region Modifiers
 
         /// <summary>
-        /// Adds the provided modifier to the StatSystem
+        /// Adds a modifier to this <see cref="StatSystem{T}"/>
         /// </summary>
         /// <param name="mod">Mod to add</param>
         public virtual void AddModifier(StatMod<T> mod)
         {
-            modifiers.Add(mod);
+            Modifiers.Add(mod);
             ModAdded?.Invoke(mod);
         }
 
         /// <summary>
-        /// Adds the provided modifiers to the StatSystem
+        /// Adds multiple modifiers to this <see cref="StatSystem{T}"/>
         /// </summary>
         /// <param name="mods">Mods to add</param>
         public virtual void AddModifiers(params StatMod<T>[] mods)
@@ -114,25 +84,22 @@ namespace Exanite.StatSystem
                 throw new ArgumentNullException(nameof(mods));
             }
 
-            foreach(StatMod<T> mod in mods)
+            foreach (StatMod<T> mod in mods)
             {
                 AddModifier(mod);
             }
         }
 
         /// <summary>
-        /// Creates a new mod, adds the mod to the StatSystem, and then returns the created mod
+        /// Creates a new mod, adds the mod to this <see cref="StatSystem{T}"/>, and then returns the created mod
         /// </summary>
         /// <param name="value">Value of the mod</param>
         /// <param name="type">How the modifier is applied to existing stats</param>
         /// <param name="source">Where the mod came from, usually "this"</param>
         /// <param name="flags">What flags the modifier has</param>
-        /// <returns>The created StatMod</returns>
-#if ODIN_INSPECTOR
+        /// <returns>The created <see cref="StatMod{T}"/></returns>
         [Button(ButtonHeight = 25, Expanded = true)]
         [TabGroup("Utility")]
-        [PropertyOrder(0)]
-#endif
         public virtual StatMod<T> AddModifier(float value, StatModType type, object source, params T[] flags)
         {
             if (flags == null)
@@ -147,10 +114,6 @@ namespace Exanite.StatSystem
             return mod;
         }
 
-        #endregion
-
-        #region Removing Mods
-
         /// <summary>
         /// Removes the provided mod from the StatSystem
         /// </summary>
@@ -158,7 +121,7 @@ namespace Exanite.StatSystem
         /// <returns>Did the mod get removed</returns>
         public virtual bool RemoveModifier(StatMod<T> mod)
         {
-            if(modifiers.Remove(mod))
+            if (Modifiers.Remove(mod))
             {
                 ModRemoved?.Invoke(mod);
                 return true;
@@ -181,7 +144,7 @@ namespace Exanite.StatSystem
 
             bool removedAll = true;
 
-            foreach(StatMod<T> mod in mods)
+            foreach (StatMod<T> mod in mods)
             {
                 if (!RemoveModifier(mod)) removedAll = false;
             }
@@ -194,21 +157,18 @@ namespace Exanite.StatSystem
         /// </summary>
         /// <param name="source">Where the mod came from, usually "this"</param>
         /// <returns>Did a mod get removed</returns>
-#if ODIN_INSPECTOR
         [Button(ButtonHeight = 25, Expanded = true)]
         [TabGroup("Utility")]
-        [PropertyOrder(1)]
-#endif
         public virtual bool RemoveAllModifiersBySource(object source)
         {
             bool didRemove = false;
 
-            for (int i = modifiers.Count - 1; i >= 0; i--)
+            for (int i = Modifiers.Count - 1; i >= 0; i--)
             {
-                if (modifiers[i].Source == source)
+                if (Modifiers[i].Source == source)
                 {
                     didRemove = true;
-                    RemoveModifier(modifiers[i]);
+                    RemoveModifier(Modifiers[i]);
                 }
             }
 
@@ -219,18 +179,15 @@ namespace Exanite.StatSystem
         /// Removes all modifiers without flag "Base", has an optional parameter to choose the exclusion flag instead
         /// </summary>
         /// <param name="flag">Modifiers with this flag will be kept</param>
-#if ODIN_INSPECTOR
         [Button(ButtonHeight = 25, Expanded = true)]
         [TabGroup("Utility")]
-        [PropertyOrder(2)]
-#endif
         public virtual void RemoveAllNonBaseModifiers()
         {
-            for (int i = modifiers.Count - 1; i >= 0; i--)
+            for (int i = Modifiers.Count - 1; i >= 0; i--)
             {
-                if (!modifiers[i].Flags.HasFlag(default(T)))
+                if (!Modifiers[i].Flags.HasFlag(default(T)))
                 {
-                    RemoveModifier(modifiers[i]);
+                    RemoveModifier(Modifiers[i]);
                 }
             }
         }
@@ -238,69 +195,120 @@ namespace Exanite.StatSystem
         /// <summary>
         /// Removes all modifiers from the StatSystem
         /// </summary>
-#if ODIN_INSPECTOR
         [Button(ButtonHeight = 25, Expanded = true)]
         [TabGroup("Utility")]
-        [PropertyOrder(3)]
-#endif
         public virtual void RemoveAllModifiers()
         {
-            for (int i = modifiers.Count - 1; i >= 0; i--)
+            for (int i = Modifiers.Count - 1; i >= 0; i--)
             {
-                RemoveModifier(modifiers[i]);
+                RemoveModifier(Modifiers[i]);
             }
         }
 
         #endregion
 
-        #endregion
-
-        #region Tracked Stats
-
-        #region Adding
+        #region TrackedStat
 
         /// <summary>
-        /// Creates and adds a new TrackedStat in the StatSystem
-        /// <param name="trackedStats">TrackedStats to track</param>
-        /// <param name="flags">Flags of this TrackedStat</param>
-#if ODIN_INSPECTOR
+        /// Adds a <see cref="TrackedStat{T}"/> to the <see cref="StatSystem{T}"/>
+        /// </summary>
+        /// <param name="name">Name of the <see cref="TrackedStat{T}"/></param>
+        /// <param name="trackedStat"><see cref="TrackedStat{T}"/> to add</param>
+        /// <returns>Added <see cref="TrackedStat{T}"/></returns>
         [Button(ButtonHeight = 25, Expanded = true)]
         [TabGroup("Utility")]
-        [PropertySpace(15)]
-        [PropertyOrder(4)]
-#endif
-        public virtual TrackedStat<T> AddTrackedStat(TrackedStat<T>[] trackedStats = null, T[] flags = null)
+        public virtual TrackedStat<T> AddTrackedStat(string name, TrackedStat<T> trackedStat)
         {
-            string name = PrepareFlagCache(trackedStats, flags);
-
-            if (this.trackedStats.ContainsKey(name))
+            if (!trackedStats.ContainsKey(name))
             {
-                throw new ArgumentException($"TrackedStat of {name} already exists in the StatSystem");
+                trackedStats.Add(name, trackedStat);
+#pragma warning disable 612, 618
+                trackedStat.OnStatSystemAdded(this, name);
+#pragma warning restore 612, 618
+                return trackedStat;
             }
             else
             {
-                this.trackedStats.Add(name, new TrackedStat<T>(this, trackedStats, flagCache.Flags));
-                return this.trackedStats[name];
+                throw new ArgumentException(nameof(name));
             }
         }
 
-        #region Internal
+        /// <summary>
+        /// Gets a <see cref="TrackedStat{T}"/> by name
+        /// </summary>
+        /// <param name="name">Name of the <see cref="TrackedStat{T}"/></param>
+        /// <returns><see cref="TrackedStat{T}"/> of name, <see langword="null"/> if not found</returns>
+        public virtual TrackedStat<T> GetTrackedStat(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("message", nameof(name));
+            }
+
+            if (trackedStats.ContainsKey(name))
+            {
+                return trackedStats[name];
+            }
+            else
+            {
+                return null;
+            }
+        }
 
         /// <summary>
-        /// Used to prepare the FlagCache before doing actions related to TrackedStat
+        /// Gets a name using the provided <see cref="TrackedStat{T}"/>, <see langword="null"/> if not found
         /// </summary>
-        /// <param name="trackedStats">TrackStats to use</param>
-        /// <param name="flags">Flags to use</param>
-        /// <returns>Name of the TrackedStat</returns>
-        protected virtual string PrepareFlagCache(TrackedStat<T>[] trackedStats, T[] flags)
+        /// <param name="trackedStat"><see cref="TrackedStat{T}"/> to use</param>
+        /// <returns>Name of the provided <see cref="TrackedStat{T}"/>, <see langword="null"/> if not found</returns>
+        public virtual string GetTrackedStatName(TrackedStat<T> trackedStat)
         {
-            flagCache.ClearFlags();
-            flagCache.SetFlags(true, flags);
-
-            BeforeTrackedStatAdded();
-
-            return TrackedStatFlagToString(trackedStats, flagCache.GetAllTrueFlags().ToArray());
+            return trackedStats.FirstOrDefault(x => x.Value == trackedStat).Key;
         }
+
+        /// <summary>
+        /// Removes a <see cref="TrackedStat{T}"/> by <paramref name="name"/>
+        /// </summary>
+        /// <param name="name">Name of the <see cref="TrackedStat{T}"/> to remove</param>
+        /// <returns>True if a <see cref="TrackedStat{T}"/> was successfully removed</returns>
+        [Button(ButtonHeight = 25, Expanded = true)]
+        [TabGroup("Utility")]
+        public virtual bool RemoveTrackedStat(string name)
+        {
+            if (trackedStats.ContainsKey(name))
+            {
+#pragma warning disable 612, 618
+                trackedStats[name].OnStatSystemRemoved();
+#pragma warning restore 612, 618
+                trackedStats.Remove(name);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Removes a <see cref="TrackedStat{T}"/> <para/>
+        /// Note: Slower than removing by name
+        /// </summary>
+        /// <param name="trackedStat"><see cref="TrackedStat{T}"/> to remove</param>
+        /// <returns>True if a <see cref="TrackedStat{T}"/> was successfully removed</returns>
+        public virtual bool RemoveTrackedStat(TrackedStat<T> trackedStat)
+        {
+            string name = GetTrackedStatName(trackedStat);
+
+            if (string.IsNullOrEmpty(name))
+            {
+                return false;
+            }
+
+            return RemoveTrackedStat(name);
+        }
+
+        #endregion
+
+        #region Internal
 
         /// <summary>
         /// Called before the TrackedStat is added to the StatSystem <para/>
@@ -314,7 +322,7 @@ namespace Exanite.StatSystem
         /// <param name="flagToAdd">Flag to add to the TrackedStat if parameters are matched</param>
         /// <param name="matchType">How to match the flags</param>
         /// <param name="flags">Flags to match with</param>
-        protected virtual void TrackedStatAddFlagCheck(T flagToAdd, FlagMatchType matchType, params T[] flags)
+        protected virtual void TrackedStatAddFlagCheck(T flagToAdd, FlagMatchType matchType, T[] flags)
         {
             if (flags == null)
             {
@@ -323,136 +331,13 @@ namespace Exanite.StatSystem
 
             for (int i = 0; i < flags.Length; i++)
             {
-                if(flagCache.HasFlag(flags[i]))
+                if (flagCache.HasFlag(flags[i]))
                 {
                     flagCache.SetFlag(true, flagToAdd);
                     break;
                 }
             }
         }
-
-        #endregion
-
-        #endregion
-
-        #region Removing
-
-        /// <summary>
-        /// Removes a TrackedStat from the StatSystem
-        /// </summary>
-        /// <param name="trackedStats">TrackedStats tracked by the target TrackedStat</param>
-        /// <param name="flags">Flags of the target TrackedStat</param>
-        /// <returns>Did the TrackedStat get removed</returns>
-#if ODIN_INSPECTOR
-        [Button(ButtonHeight = 25, Expanded = true)]
-        [TabGroup("Utility")]
-        [PropertyOrder(5)]
-#endif
-        public virtual bool RemovedTrackedStat(TrackedStat<T>[] trackedStats = null, T[] flags = null)
-        {
-            string name = PrepareFlagCache(trackedStats, flags);
-
-            if(this.trackedStats.ContainsKey(name))
-            {
-                this.trackedStats[name].UnsubscribeFromStatSystem();
-                this.trackedStats.Remove(name);
-                return true;
-            }
-
-            return false;
-        }
-
-        /// <summary>
-        /// Removes all TrackedStats from the StatSystem
-        /// </summary>
-#if ODIN_INSPECTOR
-        [Button(ButtonHeight = 25, Expanded = true)]
-        [TabGroup("Utility")]
-        [PropertyOrder(7)]
-#endif
-        public virtual void RemoveAllTrackedStats()
-        {
-            foreach (var item in trackedStats)
-            {
-                item.Value.UnsubscribeFromStatSystem();
-            }
-
-            trackedStats.Clear();
-        }
-
-        #endregion
-
-        #region Retrieving
-
-        /// <summary>
-        /// Retrieves a TrackedStat from the StatSystem
-        /// </summary>
-        /// <param name="trackedStats">TrackedStats tracked by the target TrackedStat</param>
-        /// <param name="flags">Flags of the target TrackedStat</param>
-        /// <returns>Retrieved TrackedStat</returns>
-#if ODIN_INSPECTOR
-        [Button(ButtonHeight = 25, Expanded = true)]
-        [TabGroup("Utility")]
-        [PropertyOrder(6)]
-#endif
-        public virtual TrackedStat<T> GetTrackedStat(TrackedStat<T>[] trackedStats = null, T[] flags = null)
-        {
-            string name = PrepareFlagCache(trackedStats, flags);
-
-            if (this.trackedStats.ContainsKey(name))
-            {
-                return this.trackedStats[name];
-            }
-            else
-            {
-                throw new ArgumentException($"TrackedStat of name '{name}' does not exist in the StatSystem");
-            }
-        }
-
-        #endregion
-
-        #region Internal
-
-        /// <summary>
-        /// Converts an array of TrackedStats and Ts into a string
-        /// </summary>
-        /// <param name="trackedStats">TrackedStats to convert</param>
-        /// <param name="flags">Flags to convert</param>
-        /// <returns>Converted string</returns>
-        protected virtual string TrackedStatFlagToString(TrackedStat<T>[] trackedStats, T[] flags)
-        {
-            string result = "";
-
-            if(flags != null)
-            {
-                Array.Sort(flags);
-
-                if (flags != null)
-                {
-                    for (int i = 0; i < flags.Length; i++)
-                    {
-                        result += $"{flags[i]} ";
-                    }
-                }
-            }
-            
-            if(trackedStats != null)
-            {
-                Array.Sort(trackedStats);
-
-                if (trackedStats != null)
-                {
-                    for (int i = 0; i < trackedStats.Length; i++)
-                    {
-                        result += $"({trackedStats[i].Name})";
-                    }
-                }
-            }
-
-            return result;
-        }
-
-        #endregion
 
         #endregion
     }
