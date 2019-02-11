b4e6ece292a094d18f6bf727f719ad4beedd4b2c
Exanite <Exanite78012@gmail.com>
2019-02-11 02:14:24 -0800

    Utility/PixelArt: Added a PixelArtPositioner MonoBehaviour which helps position pixel art based on number of pixels instead of units

---
+++ Utility/PixelArt/PixelArtPositioner.cs
@@ -0,0 +1,27 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Exanite.Utility.PixelArt
+{
+    public class PixelArtPositioner : MonoBehaviour
+    {
+        public int ppu = 16;
+        [SerializeField, HideInInspector]
+        private Vector2 pixelOffset;
+
+        [ShowInInspector]
+        public Vector2 PixelOffset
+        {
+            get
+            {
+                return pixelOffset;
+            }
+
+            set
+            {
+                pixelOffset = value;
+                transform.localPosition = pixelOffset / ppu;
+            }
+        }
+    } 
+}
