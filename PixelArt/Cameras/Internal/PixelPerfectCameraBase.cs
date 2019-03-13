d6e4baa2a698c5e5040337093f950d8e4b06b2c7
Exanite <Exanite78012@gmail.com>
2019-03-13 00:09:46 -0700

    PixelPerfectCameraBase.cs: Renamed from PixelPerfectCamera.cs, Changed to be a base class for other pixel perfect cameras, Moved functionality to UnitBasedPixelPerfectCamera.cs

---
+++ PixelArt/Cameras/Internal/PixelPerfectCameraBase.cs
@@ -0,0 +1,64 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Exanite.PixelArt.Cameras.Internal
+{
+    [ExecuteAlways]
+    [RequireComponent(typeof(Camera))]
+    [DisallowMultipleComponent]
+    public abstract class PixelPerfectCameraBase : MonoBehaviour
+    {
+        [SerializeField, HideInInspector]
+        private int ppu = 16;
+
+        private int screenHeight;
+        protected Camera _camera;
+
+        [ShowInInspector]
+        public int Ppu
+        {
+            get
+            {
+                return ppu;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    ppu = value;
+                }
+
+                CalculateCameraSize();
+            }
+        }
+
+        protected virtual void Start()
+        {
+            _camera = GetComponent<Camera>();
+
+            CalculateCameraSize();
+        }
+
+        protected virtual void Update()
+        {
+            if (screenHeight != Screen.height)
+            {
+                screenHeight = Screen.height;
+
+                CalculateCameraSize();
+            }
+        }
+
+        protected virtual void OnEnable()
+        {
+            if (_camera)
+            {
+                CalculateCameraSize();
+            }
+        }
+
+        [Button(ButtonHeight = 25)]
+        public abstract void CalculateCameraSize();
+    }
+}
