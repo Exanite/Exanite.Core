41d2a18e60398131ee6f9336c59487e14b01a2aa
Exanite <Exanite78012@gmail.com>
2019-03-13 00:11:22 -0700

    PixelBasedPixelPerfectCamera.cs: Added, This attempts to scale every pixel to be the specified amount of pixels (Inconsistent because of how Unity's camera works)

---
+++ PixelArt/Cameras/PixelBasedPixelPerfectCamera.cs
@@ -0,0 +1,38 @@
+﻿using Exanite.PixelArt.Cameras.Internal;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Exanite.PixelArt.Cameras
+{
+    public class PixelBasedPixelPerfectCamera : PixelPerfectCameraBase
+    {
+        [SerializeField, HideInInspector]
+        private int pixelsPerActualPixel = 10;
+
+        [ShowInInspector]
+        public int PixelsPerActualPixel
+        {
+            get
+            {
+                return pixelsPerActualPixel;
+            }
+
+            set
+            {
+                if (value != 0)
+                {
+                    pixelsPerActualPixel = value;
+                }
+
+                CalculateCameraSize();
+            }
+        }
+
+        public override void CalculateCameraSize()
+        {
+            float verticalPixels = Screen.height / PixelsPerActualPixel;
+
+            _camera.orthographicSize = verticalPixels / (Ppu * 2);
+        }
+    }
+}
