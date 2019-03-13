ea3767bc4ec49911aea865754c7e0d5bc439ec23
Exanite <Exanite78012@gmail.com>
2019-03-13 00:10:37 -0700

    UnitBasedPixelPerfectCamera.cs: Added, This has the same functionality as the old PixelPerfectCamera

---
+++ PixelArt/Cameras/UnitBasedPixelPerfectCamera.cs
@@ -0,0 +1,43 @@
+﻿using Exanite.PixelArt.Cameras.Internal;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Exanite.PixelArt.Cameras
+{
+    public class UnitBasedPixelPerfectCamera : PixelPerfectCameraBase
+    {
+        [SerializeField, HideInInspector]
+        private int verticalUnits = 10;
+
+        [ShowInInspector]
+        public int VerticalUnits
+        {
+            get
+            {
+                return verticalUnits;
+            }
+
+            set
+            {
+                if (value != 0)
+                {
+                    verticalUnits = value;
+                }
+
+                CalculateCameraSize();
+            }
+        }
+
+        public override void CalculateCameraSize()
+        {
+            int unitSize = MathE.GetNearestMultiple(Screen.height / VerticalUnits, Ppu);
+
+            if (unitSize <= 0 || unitSize == int.MaxValue)
+            {
+                unitSize = Ppu;
+            }
+
+            _camera.orthographicSize = Screen.height / (unitSize * 2f);
+        }
+    }
+}
