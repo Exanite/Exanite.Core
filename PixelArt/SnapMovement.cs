721660d6eb3ed45677b1e70f77ae96d3636f0d33
Exanite <Exanite78012@gmail.com>
2019-04-04 16:24:12 -0700

    SnapMovement.cs: Added, this uses the CameraRenderEventsHelper component to snap the positions of an object before rendering and restore it after rendering

---
+++ PixelArt/SnapMovement.cs
@@ -0,0 +1,40 @@
+﻿using Exanite.Utility;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Exanite.PixelArt
+{
+    public class SnapMovement : MonoBehaviour
+    {
+        [Required(MessageType = InfoMessageType.Error)]
+        public CameraRenderEventsHelper cameraRenderEventsHelper;
+
+        public int ppu = 16;
+
+        private Vector3 position;
+
+        private void Awake()
+        {
+            cameraRenderEventsHelper.PreRender += OnPreRender;
+            cameraRenderEventsHelper.PostRender += OnPostRender;
+        }
+
+        private void OnPreRender()
+        {
+            position = transform.position;
+
+            transform.position = new Vector3(Mathf.Round(position.x * ppu) / ppu, Mathf.Round(position.y * ppu) / ppu, Mathf.Round(position.z * ppu) / ppu);
+        }
+
+        private void OnPostRender()
+        {
+            transform.position = position;
+        }
+
+        private void OnDestroy()
+        {
+            cameraRenderEventsHelper.PreRender -= OnPreRender;
+            cameraRenderEventsHelper.PostRender -= OnPostRender;
+        }
+    } 
+}
