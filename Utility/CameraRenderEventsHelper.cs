5b6da0c17c1344f9d8e726303bce68f59b548634
Exanite <Exanite78012@gmail.com>
2019-04-04 16:00:48 -0700

    CameraRenderEventsHelper.cs: Added, this creates events for Camera render events so scripts do not have to be on the same GameObject as the camera to recieve the events

---
+++ Utility/CameraRenderEventsHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Exanite.Utility
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraRenderEventsHelper : MonoBehaviour
+    {
+        public event Action PreRender;
+        public event Action PostRender;
+
+        private void OnPreRender()
+        {
+            PreRender?.Invoke();
+        }
+
+        private void OnPostRender()
+        {
+            PostRender?.Invoke();
+        }
+    }
+}
