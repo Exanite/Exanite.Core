473c38fb2d463a1d17f8df90f7842bbef150f0a3
Exanite <Exanite78012@gmail.com>
2019-07-19 16:23:43 -0700

    Added Generic Singleton MonoBehaviour implementation

---
+++ Utility/SingletonBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Exanite.Core.Utility
+{
+    public abstract class SingletonBehaviour<T> : MonoBehaviour where T : SingletonBehaviour<T>
+    {
+        private static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    new GameObject(typeof(T).ToString(), typeof(T));
+                }
+
+                return instance;
+            }
+        }
+
+        private void Awake()
+        {
+            if (Instance)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = (T)this;
+            }
+        }
+    }
+}
