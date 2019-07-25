c631776a89f2e98f2a03177905c397139ab348cc
Exanite <Exanite78012@gmail.com>
2019-07-24 20:40:43 -0700

    Added TransformsExtensions with methods to help transform Rotations between local and world space

---
+++ Extensions/TransformExtensions.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace Exanite.Core.Extensions
+{
+    public static class TransformExtensions
+    {
+        /// <summary>
+        /// Transforms a rotation from local space to world space
+        /// </summary>
+        public static Quaternion TransformRotation(this Transform transform, Quaternion rotation)
+        {
+            var direction = rotation * Vector3.forward; // reflect then rotate
+            
+            if (transform.lossyScale.x < 0)
+            {
+                direction = Vector3.Reflect(direction, Vector3.right);
+            }
+
+            if (transform.lossyScale.y < 0)
+            {
+                direction = Vector3.Reflect(direction, Vector3.up);
+            }
+
+            if (transform.lossyScale.z < 0)
+            {
+                direction = Vector3.Reflect(direction, Vector3.forward);
+            }
+
+            rotation = Quaternion.LookRotation(direction, Vector3.up);
+            rotation = transform.rotation * rotation;
+
+            return rotation;
+        }
+
+        /// <summary>
+        /// Transforms a rotation from world space to local space. This is the opposite of <see cref="TransformRotation"/>
+        /// </summary>
+        public static Quaternion InverseTransformRotation(this Transform transform, Quaternion rotation)
+        {
+            rotation = Quaternion.Inverse(transform.rotation) * rotation; // inverse rotate then reflect
+
+            var direction = rotation * Vector3.forward;
+            
+            if (transform.lossyScale.x < 0)
+            {
+                direction = Vector3.Reflect(direction, Vector3.right);
+            }
+
+            if (transform.lossyScale.y < 0)
+            {
+                direction = Vector3.Reflect(direction, Vector3.up);
+            }
+
+            if (transform.lossyScale.z < 0)
+            {
+                direction = Vector3.Reflect(direction, Vector3.forward);
+            }
+
+            rotation = Quaternion.LookRotation(direction, Vector3.up);
+
+            return rotation;
+        }
+    }
+}
