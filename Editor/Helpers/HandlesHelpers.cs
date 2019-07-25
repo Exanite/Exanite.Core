a4efc700d49a32c7a5c86004d68e76d942b2d874
Exanite <Exanite78012@gmail.com>
2019-07-24 20:40:02 -0700

    Added DrawRectangle to HandlesHelpers

--- Editor/Helpers/HandlesHelpers.cs
+++ Editor/Helpers/HandlesHelpers.cs
@@ -5,6 +5,14 @@ namespace Exanite.Core.Editor.Helpers
 {
     public static class HandlesHelpers
     {
+        private static readonly Vector3[] Square = new Vector3[]
+        {
+            new Vector3(-0.5f, 0, -0.5f), new Vector3(-0.5f, 0, 0.5f),
+            new Vector3(-0.5f, 0, 0.5f), new Vector3(0.5f, 0, 0.5f),
+            new Vector3(0.5f, 0, 0.5f), new Vector3(0.5f, 0, -0.5f),
+            new Vector3(0.5f, 0, -0.5f), new Vector3(-0.5f, 0, -0.5f),
+        };
+
         public static void DrawLines(Vector3[] lineSegments, Vector3 position, Quaternion rotation, Vector3 scale)
         {
             for (int i = 0; i < lineSegments.Length / 2; i++)
@@ -15,5 +23,10 @@ namespace Exanite.Core.Editor.Helpers
                 Handles.DrawLine(posA, posB);
             }
         }
-    } 
+
+        public static void DrawRectangle(Vector3 position, Quaternion rotation, Vector3 scale)
+        {
+            DrawLines(Square, position, rotation, scale);
+        }
+    }
 }
