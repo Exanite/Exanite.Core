f3a5a74f7c03ac005d7770b006aa4c60162e913d
Exanite <Exanite78012@gmail.com>
2019-08-05 18:18:21 -0700

    Changed CollectionExtensions to use IEnumerables instead of IList and Array as parameters for IsNullOrEmpty Added IsEmpty

--- Extensions/CollectionExtensions.cs
+++ Extensions/CollectionExtensions.cs
@@ -1,18 +1,29 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Exanite.Core.Extensions
 {
     public static class CollectionExtensions
     {
-        public static bool IsNullOrEmpty(this Array array)
+        public static bool IsNullOrEmpty(this IEnumerable collection)
         {
-            return (array == null || array.Length == 0);
+            return collection == null || collection.IsEmpty();
         }
 
-        public static bool IsNullOrEmpty(this IList list)
+        public static bool IsEmpty(this IEnumerable collection)
         {
-            return (list == null || list.Count == 0);
+            return !collection.Cast<object>().Any();
+        }
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
+        {
+            return collection == null || collection.IsEmpty();
+        }
+
+        public static bool IsEmpty<T>(this IEnumerable<T> collection)
+        {
+            return !collection.Any();
         }
     }
 }
