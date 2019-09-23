4b89164b53fb10eba5a139b72006620645040587
Exanite <Exanite78012@gmail.com>
2019-09-22 21:32:12 -0700

    Added UnityHelper class with a SafeDestroy method

---
+++ Helpers/UnityHelper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Exanite.Core.Helpers
+{
+    /// <summary>
+    /// Miscellaneous helper class for Unity
+    /// </summary>
+    public static class UnityHelper
+    {
+        /// <summary>
+        /// Calls Destroy in Play mode and calls DestroyImmediate in Edit mode
+        /// </summary>
+        public static void SafeDestroy(Object obj)
+        {
+            if (Application.isPlaying)
+            {
+                Object.Destroy(obj);
+            }
+            else
+            {
+                Object.DestroyImmediate(obj);
+            }
+        }
+    } 
+}
