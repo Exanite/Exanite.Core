e866d8851c7a04fa4db12165bb2f88a373116432
Exanite <Exanite78012@gmail.com>
2019-11-25 21:18:44 -0800

    Added Helpers.FileHelper, only has one method right now for getting an Assets folder relative path

---
+++ Helpers/FileHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Exanite.Core.Helpers
+{
+    /// <summary>
+    /// Contains miscellaneous helper methods for managing files and paths
+    /// </summary>
+    public static class FileHelper
+    {
+#if UNITY_EDITOR
+        /// <summary>
+        /// Returns the provided path relative to the Unity assets folder
+        /// </summary>
+        /// <exception cref="ArgumentException"></exception>
+        public static string GetAssetsRelativePath(string path)
+        {
+            if (path.StartsWith(Application.dataPath))
+            {
+                return $"Assets/{path.Substring(Application.dataPath.Length).Trim('/')}";
+            }
+            else
+            {
+                throw new ArgumentException("Path does not contain the project's assets folder", nameof(path));
+            }
+        }
+#endif
+    }
+}
