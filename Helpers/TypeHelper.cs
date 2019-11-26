9bc6cc5683301bc2935089ec9e8bbf09bd83165d
Exanite <Exanite78012@gmail.com>
2019-11-25 21:20:36 -0800

    Added Helpers.TypeHelper, only has IsConcrete for now because Sirenix.Utilities.AssemblyUtilities has the functionality that was needed

---
+++ Helpers/TypeHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exanite.Core.Helpers
+{
+    /// <summary>
+    /// Contains miscellaneous helper methods for types
+    /// </summary>
+    public static class TypeHelper
+    {
+        /// <summary>
+        /// Returns true if the type is not abstract or generic
+        /// </summary>
+        public static bool IsConcrete(this Type type)
+        {
+            return !(type.IsAbstract || type.IsGenericType);
+        }
+    }
+}
