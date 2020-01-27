172209984b4253e5c9ab5ea9b4862edbd85fd3dc
Exanite <Exanite78012@gmail.com>
2020-01-27 00:02:50 -0800

    Removed the IsEmpty and IsNullOrEmpty methods. They were too ambiguous in function and too situational to be useful.

--- Extensions/CollectionExtensions.cs
+++ Extensions/CollectionExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 
 namespace Exanite.Core.Extensions
 {
@@ -9,41 +7,6 @@ namespace Exanite.Core.Extensions
     /// </summary>
     public static class CollectionExtensions
     {
-        /// <summary>
-        /// Returns if the collection is null or empty
-        /// </summary>
-        public static bool IsNullOrEmpty(this IEnumerable collection)
-        {
-            return collection == null || collection.IsEmpty();
-        }
-
-        /// <summary>
-        /// Returns if the collection is empty
-        /// </summary>
-        /// <exception cref="System.NullReferenceException"></exception>
-        public static bool IsEmpty(this IEnumerable collection)
-        {
-            return !collection.Cast<object>().Any();
-        }
-
-        /// <summary>
-        /// Returns if the collection is null or empty
-        /// </summary>
-        /// <exception cref="System.NullReferenceException"></exception>
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
-        {
-            return collection == null || collection.IsEmpty();
-        }
-
-        /// <summary>
-        /// Returns if the collection is empty
-        /// </summary>
-        /// <exception cref="System.NullReferenceException"></exception>
-        public static bool IsEmpty<T>(this IEnumerable<T> collection)
-        {
-            return !collection.Any();
-        }
-
         /// <summary>
         /// Adds a single element to the start of the collection
         /// </summary>
