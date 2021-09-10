b82244cb45481a5bde20a35b2382ebd5b3218ccc
Exanite <exanite78012@gmail.com>
2021-09-09 22:16:09 -0400

    Restore MenuItemDefines

---
+++ Editor/MenuItemDefines.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+namespace Exanite.Core.Editor
+{
+    /// <summary>
+    /// Defines all the Unity MenuItems used in this assembly
+    /// </summary>
+    internal static class MenuItemDefines
+    {
+        [MenuItem("Tools/Exanite/Core/Clean Empty Folders", priority = 10)]
+        public static void CleanEmptyFolders()
+        {
+            Editor.CleanEmptyFolders.Clean();
+        }
+    }
+}
