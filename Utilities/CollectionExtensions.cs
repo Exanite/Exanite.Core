10879903902d170b7ae0829b36d36d8289f30b41
Exanite <exanite78012@gmail.com>
2021-09-17 00:30:26 -0400

    Add IEnumerable versions of AddToStart and Add

--- Utilities/CollectionExtensions.cs
+++ Utilities/CollectionExtensions.cs
@@ -2,35 +2,52 @@
 
 namespace Exanite.Core.Utilities
 {
-    /// <summary>
-    ///     Extension methods for collections
-    /// </summary>
     public static class CollectionExtensions
     {
-        /// <summary>
-        ///     Adds a single element to the start of the collection
-        /// </summary>
-        public static IEnumerable<T> AddToStart<T>(this IEnumerable<T> collection, T element)
+        public static IEnumerable<T> AddToStart<T>(this IEnumerable<T> collection, T toStart)
         {
-            yield return element;
+            yield return toStart;
 
             foreach (var item in collection)
             {
                 yield return item;
             }
         }
+        
+        public static IEnumerable<T> AddToStart<T>(this IEnumerable<T> collection, IEnumerable<T> toStart)
+        {
+            foreach (var element in toStart)
+            {
+                yield return element;
+            }
 
-        /// <summary>
-        ///     Adds a single element to the end of the collection
-        /// </summary>
-        public static IEnumerable<T> AddToEnd<T>(this IEnumerable<T> collection, T element)
+            foreach (var element in collection)
+            {
+                yield return element;
+            }
+        }
+
+        public static IEnumerable<T> Add<T>(this IEnumerable<T> collection, T toEnd)
+        {
+            foreach (var element in collection)
+            {
+                yield return element;
+            }
+
+            yield return toEnd;
+        }
+        
+        public static IEnumerable<T> Add<T>(this IEnumerable<T> collection, IEnumerable<T> toEnd)
         {
             foreach (var item in collection)
             {
                 yield return item;
             }
 
-            yield return element;
+            foreach (var element in toEnd)
+            {
+                yield return element;
+            }
         }
     }
 }
