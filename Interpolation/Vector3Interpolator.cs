8293d017164cf1ded275e66b43ecc8be4815477f
Exanite <exanite78012@gmail.com>
2022-07-29 00:36:24 -0400

    Add Interpolator base class to Vector3Interpolator

--- Interpolation/Vector3Interpolator.cs
+++ Interpolation/Vector3Interpolator.cs
@@ -2,29 +2,11 @@
 
 namespace Exanite.Core.Interpolation
 {
-    public class Vector3Interpolator
+    public class Vector3Interpolator : Interpolator<Vector3>
     {
-        public Vector3 Current;
-        public Vector3 Previous;
-
-        private float lastPushTime;
-
-        public Vector3 Update(float time, float timeBetweenPushes, float extrapolationMultiplier = 0.5f)
+        public override Vector3 Lerp(Vector3 previous, Vector3 current, float time)
         {
-            var timeSinceLastPush = time - lastPushTime;
-            timeSinceLastPush += extrapolationMultiplier * timeBetweenPushes;
-            
-            var t = timeSinceLastPush / timeBetweenPushes;
-
-            return Vector3.LerpUnclamped(Previous, Current, t);
-        }
-
-        public void PushNext(Vector3 next, float time)
-        {
-            Previous = Current;
-            Current = next;
-
-            lastPushTime = time;
+            return Vector3.LerpUnclamped(previous, current, time);
         }
     }
 }
