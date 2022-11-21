f932a15cc72578e96c2af80199c94ca09ca3f83a
Exanite <exanite78012@gmail.com>
2022-11-21 02:22:51 -0500

    Move UiScaleHandler into Exanite.Core

---
+++ Components/UiScaleHandler.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Source.UserInterface
+{
+    public class UiScaleHandler : MonoBehaviour
+    {
+        private static readonly string ScaleName = "Project_UiScale";
+
+        public CanvasScaler Scaler;
+
+        private void Start()
+        {
+            Scaler.scaleFactor = PlayerPrefs.HasKey(ScaleName) ? PlayerPrefs.GetInt(ScaleName) : 1;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.KeypadPlus) || Input.GetKeyDown(KeyCode.Plus) || Input.GetKeyDown(KeyCode.Equals))
+            {
+                PlayerPrefs.SetInt(ScaleName, 2);
+                Scaler.scaleFactor = 2;
+            }
+
+            if (Input.GetKeyDown(KeyCode.KeypadMinus) || Input.GetKeyDown(KeyCode.Minus))
+            {
+                PlayerPrefs.SetInt(ScaleName, 1);
+                Scaler.scaleFactor = 1;
+            }
+        }
+    }
+}
