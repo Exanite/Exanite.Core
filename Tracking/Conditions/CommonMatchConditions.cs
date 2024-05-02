6b3628007ac5f7b38e2c2d537c431ec47564fab8
Exanite <exanite78012@gmail.com>
2024-05-02 08:16:44 -0400

    Add CommonMatchConditions.NoOp

--- Tracking/Conditions/CommonMatchConditions.cs
+++ Tracking/Conditions/CommonMatchConditions.cs
@@ -4,17 +4,27 @@ namespace Exanite.Core.Tracking.Conditions
 {
     public static class CommonMatchConditions
     {
+        /// <summary>
+        /// Never automatically register objects. This is useful for manually registering objects.
+        /// </summary>
+        public static bool NoOp<T>(GameObject gameObject, out T narrowedValue) where T : class
+        {
+            narrowedValue = default;
+            return false;
+        }
+
+        /// <summary>
+        /// Automatically register components on a GameObject.
+        /// </summary>
         public static bool ComponentOnGameObject<T>(GameObject gameObject, out T narrowedValue) where T : class
         {
             if (gameObject.TryGetComponent(out T display))
             {
                 narrowedValue = display;
-
                 return true;
             }
 
             narrowedValue = null;
-
             return false;
         }
     }
