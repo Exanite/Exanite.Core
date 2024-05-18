f25b3b9faa2bc35f957590b749fef7ca9b4ef4b7
Exanite <exanite78012@gmail.com>
2024-05-18 17:38:31 -0400

    Add BattleContextInstaller back (Originally UiContextAddonInstaller)

--- Utilities/TypeUtility.cs
+++ Utilities/TypeUtility.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Exanite.Core.Utilities
 {
@@ -23,6 +24,33 @@ namespace Exanite.Core.Utilities
             }
 
             return null;
+        }
+
+        /// <inheritdoc cref="GetInheritanceHierarchyUpTo"/>
+        public static IEnumerable<Type> GetInheritanceHierarchyUpTo<TBaseType>(object instance, bool isInclusive)
+        {
+            return GetInheritanceHierarchyUpTo(typeof(TBaseType), instance, isInclusive);
+        }
+
+        /// <summary>
+        /// Given an instance, this method will return the types starting from the instance's type up to the specified base type.
+        /// </summary>
+        public static IEnumerable<Type> GetInheritanceHierarchyUpTo(Type baseType, object instance, bool isInclusive)
+        {
+            var currentType = instance.GetType();
+            while (currentType != baseType)
+            {
+                if (currentType == null)
+                {
+                    throw new ArgumentException($"Object instance of type '{instance.GetType().Name}' does not inherit from base type '{baseType.Name}'", nameof(instance));
+                }
+
+                yield return currentType;
+
+                currentType = currentType.BaseType;
+            }
+
+            yield return baseType;
         }
     }
 }
