e04e93d37a349f7ae0b597bb5bdb14dc4348418e
Exanite <exanite78012@gmail.com>
2024-06-05 08:52:51 -0400

    Change TypeFilter.Self, Interfaces, InheritanceHierarchy to be extension methods

--- Types/TypeFilter.cs
+++ Types/TypeFilter.cs
@@ -7,41 +7,6 @@ namespace Exanite.Core.Types
     {
         public List<ITypeFilter> Filters { get; } = new();
 
-        public TypeFilter Self()
-        {
-            Filters.Add(TypeFilters.Self);
-
-            return this;
-        }
-
-        public TypeFilter Interfaces()
-        {
-            Filters.Add(TypeFilters.Interface);
-
-            return this;
-        }
-
-        public TypeFilter InheritanceHierarchy<TBaseType>(bool inclusive = false, bool mustExtendBaseType = false)
-        {
-            Filters.Add(new InheritanceHierarchyTypeFilter(typeof(TBaseType), inclusive, mustExtendBaseType));
-
-            return this;
-        }
-
-        public TypeFilter InheritanceHierarchy(Type baseType = null, bool inclusive = false, bool mustExtendBaseType = false)
-        {
-            if (baseType == null && inclusive == false && mustExtendBaseType == false)
-            {
-                Filters.Add(TypeFilters.InheritanceHierarchy);
-            }
-            else
-            {
-                Filters.Add(new InheritanceHierarchyTypeFilter(baseType, inclusive, mustExtendBaseType));
-            }
-
-            return this;
-        }
-
         public TypeFilter Add(ITypeFilter filter)
         {
             Filters.Add(filter);
