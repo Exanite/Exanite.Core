3cb8db4c8756d3c51c969d513d1fe88118a1c939
Exanite <exanite78012@gmail.com>
2024-06-05 18:30:44 -0400

    Add ThreadNameScope

---
+++ Threading/ThreadNameScope.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading;
+
+namespace Exanite.Core.Threading
+{
+    public struct ThreadNameScope : IDisposable
+    {
+        private readonly string originalName;
+
+        public ThreadNameScope(string name)
+        {
+            originalName = Thread.CurrentThread.Name;
+            Thread.CurrentThread.Name = name;
+        }
+
+        public void Dispose()
+        {
+            Thread.CurrentThread.Name = originalName;
+        }
+    }
+}
