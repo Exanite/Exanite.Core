0739e29c951ec85b6a89081a0128c50ab3296f4a
Exanite <exanite78012@gmail.com>
2024-06-05 08:47:53 -0400

    Add more default TypeFilter instances

--- Types/TypeFilters.cs
+++ Types/TypeFilters.cs
@@ -2,7 +2,10 @@ namespace Exanite.Core.Types
 {
     public static class TypeFilters
     {
-        public static TypeFilter Smart { get; } = new TypeFilter().InheritanceHierarchy().Interfaces();
+        public static ITypeFilter Self { get; } = new SelfTypeFilter();
+        public static ITypeFilter Interface { get; } = new InterfaceTypeFilter();
+        public static ITypeFilter InheritanceHierarchy { get; } = new InheritanceHierarchyTypeFilter();
+        public static ITypeFilter Smart { get; } = new TypeFilter().InheritanceHierarchy().Interfaces();
 
 #if UNITY_5_3_OR_NEWER
         // Todo public static TypeFilter UnitySmart { get; } = new TypeFilter().InheritanceHierarchy().Interfaces();
