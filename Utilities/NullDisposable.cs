2d21f87c0605ec5d035452a45f8a1ff897c023bc
Exanite <exanite78012@gmail.com>
2024-06-06 16:46:05 -0400

    Add docs

--- Utilities/NullDisposable.cs
+++ Utilities/NullDisposable.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Exanite.Core.Utilities
 {
+    /// <summary>
+    /// <see cref="IDisposable"/> implementation that does nothing.
+    /// </summary>
     public class NullDisposable : IDisposable
     {
         public static NullDisposable Instance { get; } = new();
