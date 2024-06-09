34b96624c5e0025958325370b31660b6d264a891
Exanite <exanite78012@gmail.com>
2024-06-09 00:52:46 -0400

    Add Vector2/3Int structs

---
+++ Numerics/Vector2Int.cs
@@ -0,0 +1,97 @@
+#if !UNITY_2021_3_OR_NEWER
+using System;
+using System.Numerics;
+
+namespace Exanite.Core.Numerics
+{
+    public struct Vector2Int : IEquatable<Vector2Int>, IFormattable
+    {
+        /// <inheritdoc cref="Vector2.X"/>
+        public int X;
+
+        /// <inheritdoc cref="Vector2.Y"/>
+        public int Y;
+
+        /// <inheritdoc cref="Vector2.Zero"/>
+        public static Vector2Int Zero => default;
+
+        /// <inheritdoc cref="Vector2.One"/>
+        public static Vector2Int One => new Vector2Int(1);
+
+        /// <inheritdoc cref="Vector2.UnitX"/>
+        public static Vector2Int UnitX => new Vector2Int(1, 0);
+
+        /// <inheritdoc cref="Vector2.UnitY"/>
+        public static Vector2Int UnitY => new Vector2Int(0, 1);
+
+        public Vector2Int(int value) : this(value, value) {}
+
+        public Vector2Int(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static explicit operator Vector2Int(Vector2 value)
+        {
+            return new Vector2Int((int)value.X, (int)value.Y);
+        }
+
+        public static implicit operator Vector2(Vector2Int value)
+        {
+            return new Vector2(value.X, value.Y);
+        }
+
+        public static Vector2Int operator +(Vector2Int left, Vector2Int right)
+        {
+            return new Vector2Int(
+                left.X + right.X,
+                left.Y + right.Y
+            );
+        }
+
+        public static Vector2Int operator -(Vector2Int left, Vector2Int right)
+        {
+            return new Vector2Int(
+                left.X - right.X,
+                left.Y - right.Y
+            );
+        }
+
+        public static Vector2Int operator -(Vector2Int value)
+        {
+            return Zero - value;
+        }
+
+        public static bool operator ==(Vector2Int left, Vector2Int right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Vector2Int left, Vector2Int right)
+        {
+            return !left.Equals(right);
+        }
+
+        public bool Equals(Vector2Int other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Vector2Int other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return ((Vector2)this).ToString(format, formatProvider);
+        }
+    }
+}
+#endif
---
+++ Numerics/Vector3Int.cs
@@ -0,0 +1,106 @@
+#if !UNITY_2021_3_OR_NEWER
+using System;
+using System.Numerics;
+
+namespace Exanite.Core.Numerics
+{
+    public struct Vector3Int : IEquatable<Vector3Int>, IFormattable
+    {
+        /// <inheritdoc cref="Vector3.X"/>
+        public int X;
+
+        /// <inheritdoc cref="Vector3.Y"/>
+        public int Y;
+
+        /// <inheritdoc cref="Vector3.Z"/>
+        public int Z;
+
+        /// <inheritdoc cref="Vector3.Zero"/>
+        public static Vector3Int Zero => default;
+
+        /// <inheritdoc cref="Vector3.One"/>
+        public static Vector3Int One => new Vector3Int(1);
+
+        /// <inheritdoc cref="Vector3.UnitX"/>
+        public static Vector3Int UnitX => new Vector3Int(1, 0, 0);
+
+        /// <inheritdoc cref="Vector3.UnitY"/>
+        public static Vector3Int UnitY => new Vector3Int(0, 1, 0);
+
+        /// <inheritdoc cref="Vector3.UnitY"/>
+        public static Vector3Int UnitZ => new Vector3Int(0, 0, 1);
+
+        public Vector3Int(int value) : this(value, value, value) {}
+
+        public Vector3Int(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public static explicit operator Vector3Int(Vector3 value)
+        {
+            return new Vector3Int((int)value.X, (int)value.Y, (int)value.Z);
+        }
+
+        public static implicit operator Vector3(Vector3Int value)
+        {
+            return new Vector3(value.X, value.Y, value.Z);
+        }
+
+        public static Vector3Int operator +(Vector3Int left, Vector3Int right)
+        {
+            return new Vector3Int(
+                left.X + right.X,
+                left.Y + right.Y,
+                left.Z + right.Z
+            );
+        }
+
+        public static Vector3Int operator -(Vector3Int left, Vector3Int right)
+        {
+            return new Vector3Int(
+                left.X - right.X,
+                left.Y - right.Y,
+                left.Z - right.Z
+            );
+        }
+
+        public static Vector3Int operator -(Vector3Int value)
+        {
+            return Zero - value;
+        }
+
+        public static bool operator ==(Vector3Int left, Vector3Int right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Vector3Int left, Vector3Int right)
+        {
+            return !left.Equals(right);
+        }
+
+        public bool Equals(Vector3Int other)
+        {
+            return X == other.X && Y == other.Y && Z == other.Z;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Vector3Int other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y, Z);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return ((Vector3)this).ToString(format, formatProvider);
+        }
+    }
+}
+#endif
