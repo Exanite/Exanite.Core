0a031f137e4d17dded65557a3f5111350f856ed1
Exanite <exanite78012@gmail.com>
2024-07-04 18:18:01 -0400

    Update test class names

--- Tests/Utilities/MathUtilityTests.Unity.cs
+++ Tests/Utilities/MathUtilityTests.Unity.cs
@@ -7,6 +7,16 @@ namespace Exanite.Core.Tests.Utilities
 {
     public partial class MathUtilityTests
     {
+        [Test]
+        public void InverseSwizzleVector3_ReversesSwizzleVector3([Values] Vector3Swizzle swizzle)
+        {
+            var original = new Vector3(1, 2, 3);
+            Vector3 result;
+
+            result = original.Swizzle(swizzle).InverseSwizzle(swizzle);
+            Assert.AreEqual(original, result);
+        }
+
         [TestCase]
         public void CreateUnityPlane_ReturnsExpectedResult1()
         {
