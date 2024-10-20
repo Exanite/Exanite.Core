07cd9de259c78613cc2d3e16d54de673b59923f6
Exanite <exanite78012@gmail.com>
2024-10-20 01:29:13 -0400

    Add UnsafeUtility.AddressOf()

---
+++ CS12/Utility/UnsafeUtility.cs
@@ -0,0 +1,20 @@
+#if NETCOREAPP
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Exanite.Core.Utility
+{
+    public static class UnsafeUtility
+    {
+        /// <summary>
+        /// Gets a pointer to a UTF-8 string literal.
+        /// See: https://github.com/dotnet/runtime/discussions/102949
+        /// </summary>
+        public static unsafe byte* AddressOf(ReadOnlySpan<byte> utf8StringLiteral)
+        {
+            return (byte*)Unsafe.AsPointer(ref Unsafe.AsRef(in MemoryMarshal.AsRef<byte>(utf8StringLiteral)));
+        }
+    }
+}
+#endif
