e27f07c3e8490b6c7341ac8b33e23c2944d91fab
Exanite <exanite78012@gmail.com>
2024-11-13 20:35:26 -0500

    Use file scoped namespace in UnsafeUtility (part of CsLatest project)

--- CsLatest/Utilities/UnsafeUtility.cs
+++ CsLatest/Utilities/UnsafeUtility.cs
@@ -3,18 +3,17 @@ using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-namespace Exanite.Core.Utilities
+namespace Exanite.Core.Utilities;
+
+public static class UnsafeUtility
 {
-    public static class UnsafeUtility
+    /// <summary>
+    /// Gets a pointer to a UTF-8 string literal.
+    /// See: https://github.com/dotnet/runtime/discussions/102949
+    /// </summary>
+    public static unsafe byte* AddressOf(ReadOnlySpan<byte> utf8StringLiteral)
     {
-        /// <summary>
-        /// Gets a pointer to a UTF-8 string literal.
-        /// See: https://github.com/dotnet/runtime/discussions/102949
-        /// </summary>
-        public static unsafe byte* AddressOf(ReadOnlySpan<byte> utf8StringLiteral)
-        {
-            return (byte*)Unsafe.AsPointer(ref Unsafe.AsRef(in MemoryMarshal.AsRef<byte>(utf8StringLiteral)));
-        }
+        return (byte*)Unsafe.AsPointer(ref Unsafe.AsRef(in MemoryMarshal.AsRef<byte>(utf8StringLiteral)));
     }
 }
 #endif
