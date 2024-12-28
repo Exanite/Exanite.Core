ca9eacc1f8eaca24cf70a7d49cfce080a086fe38
William Chen <exanite78012@gmail.com>
2024-12-28 00:40:25 -0500

    Add methods for creating orthographic and perspective matrices

--- Utilities/MathUtility.NetCore.cs
+++ Utilities/MathUtility.NetCore.cs
@@ -1,4 +1,5 @@
 #if NETCOREAPP
+using System;
 using System.Drawing;
 using System.Numerics;
 
@@ -6,6 +7,48 @@ namespace Exanite.Core.Utilities
 {
     public static partial class MathUtility
     {
+        #region Matrices
+
+        /// <summary>
+        /// Same as <see cref="Matrix4x4.CreateOrthographic"/>, but allows for reversed near and far planes.
+        /// </summary>
+        public static Matrix4x4 CreateOrthographic(float width, float height, float nearPlane, float farPlane)
+        {
+            var range = 1.0f / (nearPlane - farPlane);
+
+            return new Matrix4x4(
+                2.0f / width, 0, 0, 0,
+                0, 2.0f / height, 0, 0,
+                0, 0, range, 0,
+                0, 0, range * nearPlane, 1
+            );
+        }
+
+        /// <summary>
+        /// Same as <see cref="Matrix4x4.CreatePerspectiveFieldOfView"/>, but allows for reversed near and far planes.
+        /// </summary>
+        public static Matrix4x4 CreatePerspective(float fieldOfView, float aspectRatio, float nearPlane, float farPlane)
+        {
+            ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(fieldOfView, 0.0f);
+            ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(fieldOfView, float.Pi);
+
+            ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(nearPlane, 0.0f);
+            ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(farPlane, 0.0f);
+
+            var height = 1.0f / float.Tan(fieldOfView * 0.5f);
+            var width = height / aspectRatio;
+            var range = float.IsPositiveInfinity(farPlane) ? -1.0f : farPlane / (nearPlane - farPlane);
+
+            return new Matrix4x4(
+                width, 0, 0, 0,
+                0, height, 0, 0,
+                0, 0, range, -1.0f,
+                0, 0, range * nearPlane, 0
+            );
+        }
+
+        #endregion
+
         #region Colors
 
         public static Vector4 SrgbToLinear(string htmlColor)
