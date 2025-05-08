1a8a424d8018b4fa626a45a7f219ae0fea4334d3
William Chen <exanite78012@gmail.com>
2025-05-08 08:41:22 -0400

    Add SimplePool

---
+++ Pooling/SimplePool.cs
@@ -0,0 +1,50 @@
+namespace Exanite.Core.Pooling
+{
+    /// <inheritdoc cref="SimplePool{T}"/>>
+    public abstract class SimplePool
+    {
+        public static void Release<T>(T value) where T : new()
+        {
+            SimplePool<T>.Release(value);
+        }
+    }
+
+    /// <summary>
+    /// Pools trivially constructible objects.
+    /// <br/>
+    /// Warning: Does not handle cleanup such as object disposal or clearing.
+    /// </summary>
+    public abstract class SimplePool<T> where T : new()
+    {
+        private static readonly Pool<T> Pool = Pools.AddPool(Create(), true);
+
+        public static Pool<T> Create()
+        {
+            return new Pool<T>(create: () => new T());
+        }
+
+        public static Pool<T>.Handle Acquire(out T value)
+        {
+            lock (Pool)
+            {
+                return Pool.Acquire(out value);
+            }
+        }
+
+        public static T Acquire()
+        {
+            lock (Pool)
+            {
+                return Pool.Acquire();
+            }
+        }
+
+        public static void Release(T value)
+        {
+            lock (Pool)
+            {
+                Pool.Release(value);
+            }
+        }
+    }
+}
