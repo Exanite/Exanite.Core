74fcc9e25262578d028768f4dc56afb7bf4fa1f6
William Chen <exanite78012@gmail.com>
2025-05-09 09:23:31 -0400

    Add warning to documentation

--- CsLatest/Events/IAllEventHandler.cs
+++ CsLatest/Events/IAllEventHandler.cs
@@ -2,6 +2,14 @@ namespace Exanite.Core.Events
 {
     public interface IAllEventHandler
     {
+        // Todo: Figure out how to filter ref structs out from non ref structs. Normal type casting and pattern matching does not work.
+
+        /// <remarks>
+        /// Warning: Because of the <c>allows ref struct</c> constraint, on .NET Core, you cannot easily interact with the event's value.
+        /// Type casting and pattern matching will not work, nor will methods like ToString().
+        /// <br/>
+        /// Because of this, it's best to treat <see cref="e"/> as an opaque value.
+        /// </remarks>
         void OnEvent<T>(T e)
 #if NETCOREAPP
             where T : allows ref struct
