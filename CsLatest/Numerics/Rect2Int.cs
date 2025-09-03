a583b744d6ba95f492c41294cf4ecd338028619f
William Chen <exanite78012@gmail.com>
2025-09-03 16:01:58 -0400

    Add Rect2Int ctor

--- CsLatest/Numerics/Rect2Int.cs
+++ CsLatest/Numerics/Rect2Int.cs
@@ -2,6 +2,12 @@ namespace Exanite.Core.Numerics;
 
 public record struct Rect2Int
 {
-    public Vector2Int Offset;
     public Vector2Int Size;
+    public Vector2Int Offset;
+
+    public Rect2Int(Vector2Int size, Vector2Int offset = default)
+    {
+        Size = size;
+        Offset = offset;
+    }
 }
