bf2f584c2bd1f88b66ed3593911579db685a53f6
William Chen <exanite78012@gmail.com>
2025-09-07 23:46:35 -0400

    Begin converting APIs to use generic math

--- Utilities/MathUtility.cs
+++ Utilities/MathUtility.cs
@@ -46,7 +46,7 @@ namespace Exanite.Core.Utilities
         /// <summary>
         /// Interpolates from one value to another by <see cref="t"/>.
         /// </summary>
-        public static float LerpUnclamped(float from, float to, float t)
+        public static T LerpUnclamped<T>(T from, T to, T t) where T : INumber<T>
         {
             return from + (to - from) * t;
         }
@@ -54,41 +54,18 @@ namespace Exanite.Core.Utilities
         /// <summary>
         /// Remaps a value from one range to another.
         /// </summary>
-        public static float Remap(float value, float fromMin, float fromMax, float toMin, float toMax)
+        public static T Remap<T>(T value, T fromMin, T fromMax, T toMin, T toMax) where T : IFloatingPoint<T>
         {
             var fromRange = fromMax - fromMin;
             var toRange = toMax - toMin;
 
-            return fromRange == 0 ? toMin : toRange * ((value - fromMin) / fromRange) + toMin;
-        }
-
-        public static double Remap(double value, double fromMin, double fromMax, double toMin, double toMax)
-        {
-            var fromRange = fromMax - fromMin;
-            var toRange = toMax - toMin;
-
-            return fromRange == 0 ? toMin : toRange * ((value - fromMin) / fromRange) + toMin;
+            return fromRange == T.Zero ? toMin : toRange * ((value - fromMin) / fromRange) + toMin;
         }
 
         /// <summary>
         /// Wraps a value between min and max values.
         /// </summary>
-        public static float Wrap(float value, float min, float max)
-        {
-            return Modulo(value - min, max - min) + min;
-        }
-
-        public static double Wrap(double value, double min, double max)
-        {
-            return Modulo(value - min, max - min) + min;
-        }
-
-        public static int Wrap(int value, int min, int max)
-        {
-            return Modulo(value - min, max - min) + min;
-        }
-
-        public static long Wrap(long value, long min, long max)
+        public static T Wrap<T>(T value, T min, T max) where T : INumber<T>
         {
             return Modulo(value - min, max - min) + min;
         }
@@ -96,22 +73,7 @@ namespace Exanite.Core.Utilities
         /// <summary>
         /// Returns the true modulo of a value when divided by a divisor.
         /// </summary>
-        public static float Modulo(float value, float divisor)
-        {
-            return (value % divisor + divisor) % divisor;
-        }
-
-        public static double Modulo(double value, double divisor)
-        {
-            return (value % divisor + divisor) % divisor;
-        }
-
-        public static int Modulo(int value, int divisor)
-        {
-            return (value % divisor + divisor) % divisor;
-        }
-
-        public static long Modulo(long value, long divisor)
+        public static T Modulo<T>(T value, T divisor) where T : INumber<T>
         {
             return (value % divisor + divisor) % divisor;
         }
@@ -195,29 +157,17 @@ namespace Exanite.Core.Utilities
         /// </summary>
         /// <param name="size">Size in bytes.</param>
         /// <param name="alignment">Power of 2 alignment in bytes.</param>
-        public static int GetAlignedSize(int size, int alignment)
+        public static T GetAlignedSize<T>(T size, T alignment) where T : IBinaryInteger<T>
         {
-            return (size + alignment - 1) & ~(alignment - 1);
-        }
-
-        /// <inheritdoc cref="GetAlignedSize(int,int)"/>
-        public static uint GetAlignedSize(uint size, uint alignment)
-        {
-            return (size + alignment - 1) & ~(alignment - 1);
-        }
-
-        /// <inheritdoc cref="GetAlignedSize(int,int)"/>
-        public static ulong GetAlignedSize(ulong size, ulong alignment)
-        {
-            return (size + alignment - 1) & ~(alignment - 1);
+            return (size + alignment - T.One) & ~(alignment - T.One);
         }
 
         /// <remarks>
         /// Only valid for positive integers.
         /// </remarks>
-        public static int GreatestCommonDivisor(int a, int b)
+        public static T GreatestCommonDivisor<T>(T a, T b) where T : IBinaryInteger<T>
         {
-            while (b != 0)
+            while (b != T.Zero)
             {
                 var temp = b;
                 b = a % b;
@@ -230,7 +180,7 @@ namespace Exanite.Core.Utilities
         /// <remarks>
         /// Only valid for positive integers.
         /// </remarks>
-        public static int LeastCommonMultiple(int a, int b)
+        public static T LeastCommonMultiple<T>(T a, T b) where T : IBinaryInteger<T>
         {
             return a / GreatestCommonDivisor(a, b) * b;
         }
@@ -241,12 +191,12 @@ namespace Exanite.Core.Utilities
         /// <example>
         /// GetNearestMultiple(45, 11) will return 44.
         /// </example>
-        public static int GetNearestMultiple(int value, int multiple)
+        public static T GetNearestMultiple<T>(T value, T multiple) where T : IBinaryInteger<T>
         {
             var remainder = value % multiple;
             var result = value - remainder;
 
-            if (remainder > multiple / 2)
+            if (remainder > multiple / T.CreateTruncating(2))
             {
                 result += multiple;
             }
@@ -262,24 +212,38 @@ namespace Exanite.Core.Utilities
         /// <para/>
         /// GetNextPowerOfTwo(5) will return 8.
         /// </example>
-        public static int GetNextPowerOfTwo(int value)
+        public static T GetNextPowerOfTwo<T>(T value) where T : IBinaryInteger<T>
         {
-            return 1 << (int)Math.Log2(value - 1) + 1;
+            if (value <= T.One)
+            {
+                return T.One;
+            }
+
+            return T.One << ((int)Math.Log2(double.CreateTruncating(value - T.One)) + 1);
         }
 
-        public static bool IsEven(this int value)
+        /// <summary>
+        /// Returns true if the value is even.
+        /// </summary>
+        public static bool IsEven<T>(this T value) where T : IBinaryInteger<T>
         {
-            return value % 2 == 0;
+            return value % T.CreateTruncating(2) == T.Zero;
         }
 
-        public static bool IsOdd(this int value)
+        /// <summary>
+        /// Returns true if the value is odd.
+        /// </summary>
+        public static bool IsOdd<T>(this T value) where T : IBinaryInteger<T>
         {
-            return value % 2 != 0;
+            return value % T.CreateTruncating(2) != T.Zero;
         }
-        
-        public static bool IsPowerOfTwo(int value)
+
+        /// <summary>
+        /// Returns true if the value is a power of 2.
+        /// </summary>
+        public static bool IsPowerOfTwo<T>(this T value) where T : IBinaryInteger<T>
         {
-            return value > 0 && (value & (value - 1)) == 0;
+            return value > T.Zero && (value & (value - T.One)) == T.Zero;
         }
 
         #endregion
@@ -287,9 +251,10 @@ namespace Exanite.Core.Utilities
         #region Vectors
 
         /// <summary>
-        /// Normalizes the vector. <br/> This handles the case where the
-        /// provided vector is Vector2.Zero, returning Vector2.Zero rather
-        /// than NaN.
+        /// Normalizes the vector.
+        /// <br/>
+        /// This handles the case where the provided vector is Vector2.Zero,
+        /// returning Vector2.Zero rather than NaN.
         /// </summary>
         public static Vector2 AsNormalizedSafe(this Vector2 value)
         {
@@ -297,9 +262,10 @@ namespace Exanite.Core.Utilities
         }
 
         /// <summary>
-        /// Normalizes the vector. <br/> This handles the case where the
-        /// provided vector is Vector2.Zero, returning the provided
-        /// <see cref="fallback"/> rather than NaN.
+        /// Normalizes the vector.
+        /// <br/>
+        /// This handles the case where the provided vector is Vector2.Zero,
+        /// returning the provided <see cref="fallback"/> rather than NaN.
         /// </summary>
         public static Vector2 AsNormalizedSafe(this Vector2 value, Vector2 fallback)
         {
@@ -307,9 +273,10 @@ namespace Exanite.Core.Utilities
         }
 
         /// <summary>
-        /// Normalizes the vector. <br/> This handles the case where the
-        /// provided vector is Vector3.Zero, returning Vector3.Zero rather
-        /// than NaN.
+        /// Normalizes the vector.
+        /// <br/>
+        /// This handles the case where the provided vector is Vector3.Zero,
+        /// returning Vector3.Zero rather than NaN.
         /// </summary>
         public static Vector3 AsNormalizedSafe(this Vector3 value)
         {
@@ -317,9 +284,10 @@ namespace Exanite.Core.Utilities
         }
 
         /// <summary>
-        /// Normalizes the vector. <br/> This handles the case where the
-        /// provided vector is Vector3.Zero, returning the provided
-        /// <see cref="fallback"/> rather than NaN.
+        /// Normalizes the vector.
+        /// <br/>
+        /// This handles the case where the provided vector is Vector3.Zero,
+        /// returning the provided <see cref="fallback"/> rather than NaN.
         /// </summary>
         public static Vector3 AsNormalizedSafe(this Vector3 value, Vector3 fallback)
         {
