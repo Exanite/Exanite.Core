daa1c72d12827ce50ace7aa764294552de5b10da
William Chen <exanite78012@gmail.com>
2025-09-08 01:50:20 -0400

    Add max ray length

--- Numerics/Ray.cs
+++ Numerics/Ray.cs
@@ -5,17 +5,28 @@ namespace Exanite.Core.Numerics;
 
 public struct Ray
 {
-    public Vector3 Origin;
-    public Vector3 Direction;
-    public float Length;
+    /// <summary>
+    /// Values higher than this will be clamped down.
+    /// This is to maintain acceptable floating point accuracy.
+    /// </summary>
+    public const float MaxRayLength = 1_000_000;
+
+    public Vector3 Origin { get; set; }
+    public Vector3 Direction { get; set; }
+
+    private float length;
+    public float Length
+    {
+        readonly get => length;
+        set => length = M.Min(value, MaxRayLength);
+    }
 
     public Vector3 DirectionMagnitude => Direction * Length;
     public Vector3 End => Origin + Direction * Length;
 
-    public Ray(Vector3 origin, Vector3 direction, float length)
+    public Ray(Vector3 origin, Vector3 direction, float length = MaxRayLength)
     {
         AssertUtility.IsTrue(length >= 0, $"{nameof(length)} must be greater than or equal to 0");
-        AssertUtility.IsTrue(!float.IsInfinity(length), $"{nameof(length)} must not be infinity");
 
         Origin = origin;
         Direction = direction;
