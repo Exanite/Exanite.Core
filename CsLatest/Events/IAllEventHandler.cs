0620f1d272cdcbf2b03c7d9441aac18f72534dc0
William Chen <exanite78012@gmail.com>
2025-09-07 22:19:14 -0400

    Fix grammar issue

--- CsLatest/Events/IAllEventHandler.cs
+++ CsLatest/Events/IAllEventHandler.cs
@@ -5,7 +5,7 @@ namespace Exanite.Core.Events
         // TODO: Figure out how to filter ref structs out from non ref structs. Normal type casting and pattern matching does not work.
 
         /// <remarks>
-        /// Warning: Because of the <c>allows ref struct</c> constraint, on .NET Core, you cannot easily interact with the event's value.
+        /// Warning: Because of the <c>allows ref struct</c> constraint on .NET Core, you cannot easily interact with the event's value.
         /// Type casting and pattern matching will not work, nor will methods like ToString().
         /// <br/>
         /// Because of this, it's best to treat <see cref="e"/> as an opaque value.
