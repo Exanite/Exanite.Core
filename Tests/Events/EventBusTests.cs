a7729312f59ec8dded80a85e02a88e4afaf8391a
William Chen <exanite78012@gmail.com>
2025-09-09 12:00:15 -0400

    Add test: RegisterForwardTo_OnlyForwardsSpecifiedType

--- Tests/Events/EventBusTests.cs
+++ Tests/Events/EventBusTests.cs
@@ -7,7 +7,31 @@ namespace Exanite.Core.Tests.Events
     public class EventBusTests
     {
         [Test]
-        public void Unregister_Works_WhenUsingInterface()
+        public void RegisterForwardTo_OnlyForwardsSpecifiedType()
+        {
+            var eventBus = new EventBus();
+            var eventHandler = new AllEventHandler();
+
+            // Should receive event when registered
+            eventBus.RegisterForwardTo<Event>(eventHandler);
+            eventBus.Raise(new Event());
+
+            Assert.That(eventHandler.ReceiveCount, Is.EqualTo(1));
+
+            // Should not receive unregistered event
+            eventBus.Raise(new EventB());
+
+            Assert.That(eventHandler.ReceiveCount, Is.EqualTo(1));
+
+            // Should not receive event after unregistering
+            eventBus.UnregisterForwardTo<Event>(eventHandler);
+            eventBus.Raise(new Event());
+
+            Assert.That(eventHandler.ReceiveCount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Unregister_SuccessfullyUnregisters_WhenUsingInterface()
         {
             var eventBus = new EventBus();
             var eventHandler = new EventHandler<Event>();
@@ -26,12 +50,23 @@ namespace Exanite.Core.Tests.Events
         }
 
         private struct Event;
+        private struct EventB;
 
         private class EventHandler<T> : IEventHandler<T>
         {
             public int ReceiveCount { get; private set; }
 
             public void OnEvent(T e)
+            {
+                ReceiveCount++;
+            }
+        }
+
+        private class AllEventHandler : IAllEventHandler
+        {
+            public int ReceiveCount { get; private set; }
+
+            public void OnEvent<T>(T e) where T : allows ref struct
             {
                 ReceiveCount++;
             }
