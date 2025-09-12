22a06f5661b1e6f4470a2f0042417838501e2f8e
William Chen <exanite78012@gmail.com>
2025-09-12 02:18:42 -0400

    Add FileUtility.AreFilesEqual()

--- Utilities/FileUtility.cs
+++ Utilities/FileUtility.cs
@@ -1,5 +1,7 @@
 ﻿using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using Exanite.Core.Io;
 
 namespace Exanite.Core.Utilities;
 
@@ -14,5 +16,28 @@ public static class FileUtility
     public static bool IsEmpty(this DirectoryInfo folder)
     {
         return !folder.EnumerateFileSystemInfos().Any();
+    }
+
+    /// <summary>
+    /// Checks if the files are equal by comparing their contents. Throws if either file does not exist.
+    /// </summary>
+    public static bool AreFilesEqual(AbsolutePath pathA, AbsolutePath pathB)
+    {
+        var fileInfo1 = new FileInfo(pathA);
+        var fileInfo2 = new FileInfo(pathB);
+
+        if (fileInfo1.Length != fileInfo2.Length)
+        {
+            return false;
+        }
+
+        using var sha256 = SHA256.Create();
+        using var stream1 = File.OpenRead(pathA);
+        using var stream2 = File.OpenRead(pathB);
+
+        var hash1 = sha256.ComputeHash(stream1);
+        var hash2 = sha256.ComputeHash(stream2);
+
+        return hash1.SequenceEqual(hash2);
     }
 }
