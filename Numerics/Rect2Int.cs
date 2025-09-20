cee8a15b659ca51586f4009836ce72d47ffbba60
William Chen <exanite78012@gmail.com>
2025-09-19 20:23:53 -0400

    Add cast operators to Rect structs

--- Numerics/Rect2Int.cs
+++ Numerics/Rect2Int.cs
@@ -10,5 +10,15 @@ public record struct Rect2Int
         Size = size;
         Offset = offset;
     }
+
+    public static explicit operator Rect2Int(Rect2 rect)
+    {
+        return new Rect2Int((Vector2Int)rect.Size, (Vector2Int)rect.Offset);
+    }
+
+    public static implicit operator Rect2(Rect2Int value)
+    {
+        return new Rect2(value.Size, value.Offset);
+    }
 }
 
--- Numerics/Rect3Int.cs
+++ Numerics/Rect3Int.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace Exanite.Core.Numerics;
 
 public record struct Rect3Int
@@ -10,5 +12,16 @@ public record struct Rect3Int
         Size = size;
         Offset = offset;
     }
+
+    public static explicit operator Rect3Int(Rect2 rect)
+    {
+        return new Rect3Int(new Vector3Int((int)rect.Size.X, (int)rect.Size.Y, 0), new Vector3Int((int)rect.Offset.X, (int)rect.Offset.Y, 0));
+    }
+
+    public static implicit operator Rect2(Rect3Int value)
+    {
+        return new Rect2(new Vector2(value.Size.X, value.Size.Y), new Vector2(value.Offset.X, value.Offset.Y));
+    }
+
 }
 
