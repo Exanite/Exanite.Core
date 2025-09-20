c3837490977ee4e1529b932f67e6713b13446867
William Chen <exanite78012@gmail.com>
2025-09-20 02:02:34 -0400

    Fix incorrect Rect3Int cast operators

--- Numerics/Rect3Int.cs
+++ Numerics/Rect3Int.cs
@@ -13,14 +13,14 @@ public record struct Rect3Int
         Offset = offset;
     }
 
-    public static explicit operator Rect3Int(Rect2 rect)
+    public static explicit operator Rect3Int(Rect3 rect)
     {
-        return new Rect3Int(new Vector3Int((int)rect.Size.X, (int)rect.Size.Y, 0), new Vector3Int((int)rect.Offset.X, (int)rect.Offset.Y, 0));
+        return new Rect3Int(new Vector3Int((int)rect.Size.X, (int)rect.Size.Y, (int)rect.Size.Z), new Vector3Int((int)rect.Offset.X, (int)rect.Offset.Y, (int)rect.Offset.Z));
     }
 
-    public static implicit operator Rect2(Rect3Int value)
+    public static implicit operator Rect3(Rect3Int value)
     {
-        return new Rect2(new Vector2(value.Size.X, value.Size.Y), new Vector2(value.Offset.X, value.Offset.Y));
+        return new Rect3(new Vector3(value.Size.X, value.Size.Y, value.Size.Z), new Vector3(value.Offset.X, value.Offset.Y, value.Offset.Z));
     }
 
     public readonly bool Contains(Vector3 position)
