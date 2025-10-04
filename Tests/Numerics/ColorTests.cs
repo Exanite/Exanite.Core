3d0379ef131dd307943415d8a2c913235c0e3ffd
William Chen <exanite78012@gmail.com>
2025-10-04 18:18:47 -0400

    Add additional test

--- Tests/Numerics/ColorTests.cs
+++ Tests/Numerics/ColorTests.cs
@@ -18,6 +18,8 @@ public class ColorTests
             Assert.That(Color.FromHsl(180, 0, 1).ToHex(false), Is.EqualTo("#FFFFFF"));
             Assert.That(Color.FromHsl(180, 0, 0).ToHex(false), Is.EqualTo("#000000"));
 
+            Assert.That(Color.FromHsl(0, 1, 0.5f).ToHex(false), Is.EqualTo("#FF0000"));
+
             Assert.That(Color.FromHsl(213, 0.605f, 0.318f).ToHex(false), Is.EqualTo("#204C82"));
             Assert.That(Color.FromHsl(121, 0.313f, 0.617f).ToHex(false), Is.EqualTo("#7FBC80"));
             Assert.That(Color.FromHsl(25, 0.890f, 0.209f).ToHex(false), Is.EqualTo("#652D06"));
