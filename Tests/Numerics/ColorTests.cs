e68dfc945554a9956ad2c902b7167ea59c5434b2
William Chen <exanite78012@gmail.com>
2025-10-04 18:03:30 -0400

    Adjust test cases

--- Tests/Numerics/ColorTests.cs
+++ Tests/Numerics/ColorTests.cs
@@ -11,15 +11,21 @@ public class ColorTests
     {
         Assert.Multiple(() =>
         {
-            Assert.That(Color.FromHsl(0, 0, 1).Srgb, Is.EqualTo(Color.FromHex("#FFFFFF").Srgb));
-            Assert.That(Color.FromHsl(0, 0, 0).Srgb, Is.EqualTo(Color.FromHex("#000000").Srgb));
+            Assert.That(Color.FromHsl(0, 0, 1).Srgb, Is.EqualTo(Color.FromHex("#FFFFFF").Srgb).Using<Color>(ColorComparer));
+            Assert.That(Color.FromHsl(0, 0, 1).Srgb, Is.EqualTo(Color.FromHex("#FFFFFF").Srgb).Using<Color>(ColorComparer));
+            Assert.That(Color.FromHsl(0, 0, 0).Srgb, Is.EqualTo(Color.FromHex("#000000").Srgb).Using<Color>(ColorComparer));
 
-            Assert.That(Color.FromHsl(180, 0, 1).Srgb, Is.EqualTo(Color.FromHex("#FFFFFF").Srgb));
-            Assert.That(Color.FromHsl(180, 0, 0).Srgb, Is.EqualTo(Color.FromHex("#000000").Srgb));
+            Assert.That(Color.FromHsl(180, 0, 1).Srgb, Is.EqualTo(Color.FromHex("#FFFFFF").Srgb).Using<Color>(ColorComparer));
+            Assert.That(Color.FromHsl(180, 0, 0).Srgb, Is.EqualTo(Color.FromHex("#000000").Srgb).Using<Color>(ColorComparer));
 
-            Assert.That(Color.FromHsl(213, 0.605f, 0.318f).Srgb, Is.EqualTo(Color.FromHex("#204C82").Srgb));
-            Assert.That(Color.FromHsl(121, 0.313f, 0.617f).Srgb, Is.EqualTo(Color.FromHex("#7FBC80").Srgb));
-            Assert.That(Color.FromHsl(25, 0.890f, 0.209f).Srgb, Is.EqualTo(Color.FromHex("#652D06").Srgb));
+            Assert.That(Color.FromHsl(213, 0.605f, 0.318f).Srgb, Is.EqualTo(Color.FromHex("#204C82").Srgb).Using<Color>(ColorComparer));
+            Assert.That(Color.FromHsl(121, 0.313f, 0.617f).Srgb, Is.EqualTo(Color.FromHex("#7FBC80").Srgb).Using<Color>(ColorComparer));
+            Assert.That(Color.FromHsl(25, 0.890f, 0.209f).Srgb, Is.EqualTo(Color.FromHex("#652D06").Srgb).Using<Color>(ColorComparer));
         });
+    }
+
+    private static bool ColorComparer(Color a, Color b)
+    {
+        return a.IsApproximatelyEqual(b);
     }
 }
