cfdfa737adc8704d744f9d6dc924e396f2f80ae0
William Chen <exanite78012@gmail.com>
2025-11-07 04:15:07 -0500

    Change DoubleBuffer to be a class

--- Runtime/DoubleBuffer.cs
+++ Runtime/DoubleBuffer.cs
@@ -3,10 +3,16 @@ namespace Exanite.Core.Runtime;
 /// <summary>
 /// Simple double buffer implementation.
 /// </summary>
-public struct DoubleBuffer<T>
+public class DoubleBuffer<T>
 {
     public T Read { get; private set; }
     public T Write { get; private set; }
+
+    public DoubleBuffer(T read, T write)
+    {
+        Read = read;
+        Write = write;
+    }
 
     /// <summary>
     /// Swap the read and write resources.
