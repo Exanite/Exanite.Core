4fb00f20e8f65b2fa61e084eb9c9e278e8af679b
William Chen <exanite78012@gmail.com>
2025-11-07 01:53:21 -0500

    Add ability to specify exception handler for TaskUtility.Forget()

--- Utilities/TaskUtility.cs
+++ Utilities/TaskUtility.cs
@@ -5,15 +5,84 @@ namespace Exanite.Core.Utilities;
 
 public static class TaskUtility
 {
-    public static async void Forget(this Task task)
+    public static Action<Exception> DefaultExceptionHandlerAction { get; set; } = DefaultExceptionHandler;
+
+    public static void Forget(this Task task)
     {
-        try
+        task.Forget(DefaultExceptionHandlerAction);
+    }
+
+    public static void Forget<T>(this Task<T> task)
+    {
+        task.Forget(DefaultExceptionHandlerAction);
+    }
+
+    public static void Forget(this Task task, Action<Exception> exceptionHandler)
+    {
+        var awaiter = task.GetAwaiter();
+        if (awaiter.IsCompleted)
         {
-            await task;
+            try
+            {
+                awaiter.GetResult();
+            }
+            catch (Exception e)
+            {
+                exceptionHandler.Invoke(e);
+            }
         }
-        catch (Exception e)
+        else
         {
-            Console.Error.WriteLine(e);
+            var capturedAwaiter = awaiter;
+            var capturedExceptionHandler = exceptionHandler;
+            capturedAwaiter.OnCompleted(() =>
+            {
+                try
+                {
+                    capturedAwaiter.GetResult();
+                }
+                catch (Exception e)
+                {
+                    capturedExceptionHandler.Invoke(e);
+                }
+            });
         }
+    }
+
+    public static void Forget<T>(this Task<T> task, Action<Exception> exceptionHandler)
+    {
+        var awaiter = task.GetAwaiter();
+        if (awaiter.IsCompleted)
+        {
+            try
+            {
+                awaiter.GetResult();
+            }
+            catch (Exception e)
+            {
+                exceptionHandler.Invoke(e);
+            }
+        }
+        else
+        {
+            var capturedAwaiter = awaiter;
+            var capturedExceptionHandler = exceptionHandler;
+            capturedAwaiter.OnCompleted(() =>
+            {
+                try
+                {
+                    capturedAwaiter.GetResult();
+                }
+                catch (Exception e)
+                {
+                    capturedExceptionHandler.Invoke(e);
+                }
+            });
+        }
+    }
+
+    private static void DefaultExceptionHandler(Exception e)
+    {
+        Console.WriteLine(e);
     }
 }
