55b8b4bc10ad2d119138bef1751973eb367b48cf
William Chen <exanite78012@gmail.com>
2025-12-16 21:53:15 -0500

    Add additional docs to Assert/GuardException

--- Runtime/AssertException.cs
+++ Runtime/AssertException.cs
@@ -6,6 +6,11 @@ namespace Exanite.Core.Runtime;
 /// <summary>
 /// Exception thrown by <see cref="AssertUtility"/>.
 /// </summary>
+/// <remarks>
+/// This exception is not meant to be caught.
+/// If this exception is thrown, it implies that the entire program
+/// is potentially in an invalid state and execution should not continue.
+/// </remarks>
 public class AssertException : Exception
 {
     public AssertException(string message) : base(message) {}
--- Runtime/GuardException.cs
+++ Runtime/GuardException.cs
@@ -6,6 +6,11 @@ namespace Exanite.Core.Runtime;
 /// <summary>
 /// Exception thrown by <see cref="GuardUtility"/>.
 /// </summary>
+/// <remarks>
+/// This exception is not meant to be caught.
+/// If this exception is thrown, it implies that the entire program
+/// is potentially in an invalid state and execution should not continue.
+/// </remarks>
 public class GuardException : Exception
 {
     public GuardException(string message) : base(message) {}
