b4ce8085ebdb78f0aa2d6d827c9434a391afab09
William Chen <exanite78012@gmail.com>
2026-01-06 20:30:38 -0500

    Add convenience constructor to DoubleBuffer class and improve docs

--- Runtime/DoubleBuffer.cs
+++ Runtime/DoubleBuffer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 
 namespace Exanite.Core.Runtime;
@@ -16,6 +17,9 @@ public static class DoubleBuffer
 /// <summary>
 /// Simple double buffer implementation.
 /// </summary>
+/// <remarks>
+/// This class is intentionally not disposable.
+/// </remarks>
 public class DoubleBuffer<T>
 {
     private readonly Lock sync = new();
@@ -30,10 +34,25 @@ public class DoubleBuffer<T>
     /// </summary>
     public T Write { get; private set; }
 
+    /// <summary>
+    /// Create a double buffer with the provided resources.
+    /// </summary>
+    /// <param name="read">The resource that will be first used as the read resource.</param>
+    /// <param name="write">The resource that will be first used as the write resource.</param>
     public DoubleBuffer(T read, T write)
     {
         Read = read;
         Write = write;
+    }
+
+    /// <summary>
+    /// Create a double buffer using the resources created by the function to create the read and write resources.
+    /// </summary>
+    /// <param name="create">Create the resource. The int parameter is the number of resources created so far.</param>
+    public DoubleBuffer(Func<int, T> create)
+    {
+        Read = create.Invoke(0);
+        Write = create.Invoke(1);
     }
 
     /// <summary>
