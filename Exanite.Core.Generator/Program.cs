0a572894512a3e7e0333e1ad02e9e955cff7dae3
William Chen <exanite78012@gmail.com>
2026-03-07 11:41:55 -0500

    Add code generator project
    Yes, it depends on the same project it's meant to generate code for. I'm just going to live with that fact.

---
+++ Exanite.Core.Generator/Program.cs
@@ -0,0 +1,13 @@
+using Exanite.Core.Io;
+using Exanite.Core.Utilities;
+
+namespace Exanite.Core.Generator;
+
+public class Program
+{
+    public static void Main()
+    {
+        var csprojFile = AbsolutePath.WorkingDirectory / "Exanite.Core" / "Exanite.Core.csproj";
+        GuardUtility.IsTrue(csprojFile.Exists, "Working directory is incorrect. Please set it to be the root of the Exanite.Core repo");
+    }
+}
