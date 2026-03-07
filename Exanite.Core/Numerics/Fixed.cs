098b7adc68e56547746dadaeae3505e041411a60
William Chen <exanite78012@gmail.com>
2026-03-07 14:49:14 -0500

    Implement IEquatable and IComparable

--- Exanite.Core/Numerics/Fixed.cs
+++ Exanite.Core/Numerics/Fixed.cs
@@ -1,9 +1,11 @@
+using System;
+
 namespace Exanite.Core.Numerics;
 
 /// <summary>
-/// A fixed point Q48.16 value (48-bits for integer, 16-bits for fraction).
+/// A fixed point Q48.16 value (48-bits integer, 16-bits fraction).
 /// </summary>
-public readonly struct Fixed
+public readonly struct Fixed : IEquatable<Fixed>, IComparable<Fixed>
 {
     private readonly long value;
 
@@ -11,4 +13,18 @@ public readonly struct Fixed
     {
         this.value = value;
     }
+
+    // Comparisons
+    public static bool operator ==(Fixed left, Fixed right) => left.value == right.value;
+    public static bool operator !=(Fixed left, Fixed right) => left.value != right.value;
+    public static bool operator <(Fixed left, Fixed right) => left.value < right.value;
+    public static bool operator >(Fixed left, Fixed right) => left.value > right.value;
+    public static bool operator <=(Fixed left, Fixed right) => left.value <= right.value;
+    public static bool operator >=(Fixed left, Fixed right) => left.value >= right.value;
+
+    public int CompareTo(Fixed other) => value.CompareTo(other.value);
+
+    public override bool Equals(object? obj) => obj is Fixed other && Equals(other);
+    public bool Equals(Fixed other) => value == other.value;
+    public override int GetHashCode() => value.GetHashCode();
 }
