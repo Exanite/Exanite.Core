b31d113fb457442ba759589e0323aac3b80c9c7f
William Chen <exanite78012@gmail.com>
2026-03-09 00:50:37 -0400

    Switch to Q88.40 for better precision
    Might revert to Q96.32 since I'm not sure of the performance cost, but this revealed a few mistakes in the shift calculation that were masked by the fact that internalShift - Shift = Shift when internalShift = 32.

--- Exanite.Core/Numerics/Fixed.Root.cs
+++ Exanite.Core/Numerics/Fixed.Root.cs
@@ -21,43 +21,41 @@ public partial struct Fixed // : IRootFunctions<Fixed>
             return 0;
         }
 
-        // checked // Uncomment when debugging
+        // This uses Q88.40 for better precision
+        const int internalShift = 40;
+
+        // Normalize x using an even shift so that the shift can be safely halved later
+        // This leads to x being in the interval [0.5, 2)
+        var leadingZeroCount = (int)long.LeadingZeroCount(x.value) + 64;
+        var evenNormalizeShift = (leadingZeroCount - (128 - 1 - internalShift)) & ~1;
+        var normalizedX = evenNormalizeShift >= 0 ? (Int128)x.value << evenNormalizeShift : (Int128)x.value >> -evenNormalizeShift;
+
+        // Calculate LUT index of initial guess
+        // 2 is represented with 41 + 2 bits, but we are exclusive of 2
+        const int availableBitCount = internalShift + 2 - 1;
+        var lutIndex = (int)(normalizedX >> (availableBitCount - SqrtLutBits));
+        var y = (Int128)SqrtLut[lutIndex - SqrtLutOffset] << (internalShift - Shift);
+
+        var three = (Int128)3 << internalShift;
+        while (true)
         {
-            // This uses Q96.32 for better precision
-            // TODO: 96 is probably extremely excessive since the values are normalized
-
-            // Normalize x using an even shift so that the shift can be safely halved later
-            // This leads to x being in the interval [0.5, 2)
-            var leadingZeroCount = (int)long.LeadingZeroCount(x.value) + 64;
-            var evenNormalizeShift = (leadingZeroCount - (128 - 1 - Shift * 2)) & ~1;
-            var normalizedX = evenNormalizeShift >= 0 ? (Int128)x.value << evenNormalizeShift : (Int128)x.value >> -evenNormalizeShift;
-
-            // Calculate LUT index of initial guess
-            // 2 is represented with 32 + 2 bits, but we are exclusive of 2
-            const int availableBitCount = 32 + 2 - 1;
-            var lutIndex = (int)(normalizedX >> (availableBitCount - SqrtLutBits));
-            var y = (Int128)SqrtLut[lutIndex - SqrtLutOffset] << Shift;
-
-            while (true)
+            // Inverse Newton-Raphson method:
+            // y_n+1 = (y_n * (3 - x * y_n * y_n)) >> 1
+            var xyy = (normalizedX * y * y) >> (internalShift * 2);
+            var threeMinusXyy = three - xyy;
+            var yNext = (y * threeMinusXyy) >> (internalShift + 1);
+            if (yNext == y)
             {
-                // Inverse Newton-Raphson method:
-                // y_n+1 = (y_n * (3 - x * y_n * y_n)) >> 1
-                var xyy = (normalizedX * y * y) >> (Shift * 4);
-                var threeMinusXyy = ((Int128)3 << (Shift * 2)) - xyy;
-                var yNext = (y * threeMinusXyy) >> (Shift * 2 + 1);
-                if (yNext == y)
-                {
-                    break;
-                }
-
-                y = yNext;
+                break;
             }
 
-            // We need to cancel out the normalization step we did above
-            var normalizedResult = (normalizedX * y) >> (Shift * 2);
-            var finalShift = (evenNormalizeShift - Shift) / 2;
-            var fixed128Value = finalShift >= 0 ? normalizedResult >> finalShift : normalizedResult << -finalShift;
-            return new Fixed((long)(fixed128Value >> Shift));
+            y = yNext;
         }
+
+        // We need to cancel out the normalization step we did above
+        var normalizedResult = (normalizedX * y) >> internalShift;
+        var finalShift = (evenNormalizeShift - (internalShift - Shift)) / 2;
+        var fixed128Value = finalShift >= 0 ? normalizedResult >> finalShift : normalizedResult << -finalShift;
+        return new Fixed((long)(fixed128Value >> (internalShift - Shift)));
     }
 }
