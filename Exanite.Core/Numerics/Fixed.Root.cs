cb25f32c2ff53bf411edb253213771ea34851a16
William Chen <exanite78012@gmail.com>
2026-03-10 00:10:25 -0400

    Implement Hypot (note the current impl can easily overflow, but to fix it, we need an int128 version of sqrt)
    Wait... an int128 version might be easy enough...

--- Exanite.Core/Numerics/Fixed.Root.cs
+++ Exanite.Core/Numerics/Fixed.Root.cs
@@ -6,7 +6,6 @@ namespace Exanite.Core.Numerics;
 public partial struct Fixed // : IRootFunctions<Fixed>
 {
     // public static Fixed Cbrt(Fixed x);
-    // public static Fixed Hypot(Fixed x, Fixed y);
     // public static Fixed RootN(Fixed x, int n);
 
     public static Fixed Sqrt(Fixed x)
@@ -58,5 +57,32 @@ public partial struct Fixed // : IRootFunctions<Fixed>
         var finalShift = (evenNormalizeShift - (internalShift - Shift)) / 2;
         var fixed128Value = finalShift >= 0 ? normalizedResult >> finalShift : normalizedResult << -finalShift;
         return new Fixed((long)(fixed128Value >> (internalShift - Shift)));
+    }
+
+    public static Fixed Hypot(Fixed x, Fixed y)
+    {
+        var x2 = (Int128)x.raw * x.raw;
+        var y2 = (Int128)y.raw * y.raw;
+        var sum = (x2 + y2) >> Shift;
+        return Sqrt(new Fixed((long)sum));
+    }
+
+    public static Fixed Hypot(Fixed x, Fixed y, Fixed z)
+    {
+        var x2 = (Int128)x.raw * x.raw;
+        var y2 = (Int128)y.raw * y.raw;
+        var z2 = (Int128)z.raw * z.raw;
+        var sum = (x2 + y2 + z2) >> Shift;
+        return Sqrt(new Fixed((long)sum));
+    }
+
+    public static Fixed Hypot(Fixed x, Fixed y, Fixed z, Fixed w)
+    {
+        var x2 = (Int128)x.raw * x.raw;
+        var y2 = (Int128)y.raw * y.raw;
+        var z2 = (Int128)z.raw * z.raw;
+        var w2 = (Int128)w.raw * w.raw;
+        var sum = (x2 + y2 + z2 + w2) >> Shift;
+        return Sqrt(new Fixed((long)sum));
     }
 }
