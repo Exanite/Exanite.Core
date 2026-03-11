668329f684019dcf61199addd3e71a44c577de13
William Chen <exanite78012@gmail.com>
2026-03-11 15:16:55 -0400

    Use checked scope for Hypot
    (cherry picked from commit fae647071f2949bd52ebe9e7968bceb253cfabee)

--- Exanite.Core/Numerics/Fixed.Root.cs
+++ Exanite.Core/Numerics/Fixed.Root.cs
@@ -53,28 +53,37 @@ public partial struct Fixed
 
     public static Fixed Hypot(Fixed x, Fixed y)
     {
-        var x2 = (Int128)x.Raw * x.Raw;
-        var y2 = (Int128)y.Raw * y.Raw;
-        var sum = x2 + y2;
-        return (Fixed)Fixed128.SqrtFast(new Fixed128(sum));
+        checked
+        {
+            var x2 = (Int128)x.Raw * x.Raw;
+            var y2 = (Int128)y.Raw * y.Raw;
+            var sum = x2 + y2;
+            return (Fixed)Fixed128.SqrtFast(new Fixed128(sum));
+        }
     }
 
     public static Fixed Hypot(Fixed x, Fixed y, Fixed z)
     {
-        var x2 = (Int128)x.Raw * x.Raw;
-        var y2 = (Int128)y.Raw * y.Raw;
-        var z2 = (Int128)z.Raw * z.Raw;
-        var sum = x2 + y2 + z2;
-        return (Fixed)Fixed128.SqrtFast(new Fixed128(sum));
+        checked
+        {
+            var x2 = (Int128)x.Raw * x.Raw;
+            var y2 = (Int128)y.Raw * y.Raw;
+            var z2 = (Int128)z.Raw * z.Raw;
+            var sum = x2 + y2 + z2;
+            return (Fixed)Fixed128.SqrtFast(new Fixed128(sum));
+        }
     }
 
     public static Fixed Hypot(Fixed x, Fixed y, Fixed z, Fixed w)
     {
-        var x2 = (Int128)x.Raw * x.Raw;
-        var y2 = (Int128)y.Raw * y.Raw;
-        var z2 = (Int128)z.Raw * z.Raw;
-        var w2 = (Int128)w.Raw * w.Raw;
-        var sum = x2 + y2 + z2 + w2;
-        return (Fixed)Fixed128.SqrtFast(new Fixed128(sum));
+        checked
+        {
+            var x2 = (Int128)x.Raw * x.Raw;
+            var y2 = (Int128)y.Raw * y.Raw;
+            var z2 = (Int128)z.Raw * z.Raw;
+            var w2 = (Int128)w.Raw * w.Raw;
+            var sum = x2 + y2 + z2 + w2;
+            return (Fixed)Fixed128.SqrtFast(new Fixed128(sum));
+        }
     }
 }
