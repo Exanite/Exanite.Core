26434a7c4f07f8bf188940c2bc99594a171248c6
William Chen <exanite78012@gmail.com>
2026-03-12 02:32:59 -0400

    Copy over the basic tests from Fixed to Fixed128

--- Exanite.Core.Tests/Numerics/Fixed128BasicTests.cs
+++ Exanite.Core.Tests/Numerics/Fixed128BasicTests.cs
@@ -6,6 +6,64 @@ namespace Exanite.Core.Tests.Numerics;
 
 public class Fixed128BasicTests
 {
+    [Fact]
+    public void MinMax_AreSymmetric()
+    {
+        Assert.Equal(Fixed128.MaxValue, -Fixed128.MinValue);
+    }
+
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(0, 0)]
+    [InlineData(-1, -1)]
+    [InlineData(123.456, 1)]
+    [InlineData(-123.456, -1)]
+    public void Sign_ReturnsExpectedValue(double input, int expected)
+    {
+        Assert.Equal(expected, (int)Fixed128.Sign((Fixed128)input));
+    }
+
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(0, 1)]
+    [InlineData(-1, -1)]
+    [InlineData(123.456, 1)]
+    [InlineData(-123.456, -1)]
+    public void SignNonZero_ReturnsExpectedValue(double input, int expected)
+    {
+        Assert.Equal(expected, (int)Fixed128.SignNonZero((Fixed128)input));
+    }
+
+    [Theory]
+    [InlineData(1.5, 1)]
+    [InlineData(1.4, 1)]
+    [InlineData(3.14159, 3)]
+    [InlineData(-3.5, -4)]
+    public void Floor_ReturnsExpectedValue(double input, int expected)
+    {
+        Assert.Equal(expected, (int)Fixed128.Floor((Fixed128)input));
+    }
+
+    [Theory]
+    [InlineData(1.5, 2)]
+    [InlineData(1.4, 2)]
+    [InlineData(3.14159, 4)]
+    [InlineData(-3.5, -3)]
+    public void Ceiling_ReturnsExpectedValue(double input, int expected)
+    {
+        Assert.Equal(expected, (int)Fixed128.Ceiling((Fixed128)input));
+    }
+
+    [Theory]
+    [InlineData(1.5, 2)]
+    [InlineData(1.4, 1)]
+    [InlineData(3.14159, 3)]
+    [InlineData(-3.5, -4)]
+    public void Round_ReturnsExpectedValue(double input, int expected)
+    {
+        Assert.Equal(expected, (int)Fixed128.Round((Fixed128)input));
+    }
+
     [Theory]
     // ToEven
     [InlineData(1.5, MidpointRounding.ToEven, 2)]
