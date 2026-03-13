db7490f1bf4f95f2127db59ffa55c48f341fc8ee
William Chen <exanite78012@gmail.com>
2026-03-13 19:26:17 -0400

    Work on formatting

--- Exanite.Core/Numerics/Fixed128.Format.cs
+++ Exanite.Core/Numerics/Fixed128.Format.cs
@@ -13,18 +13,19 @@ public partial struct Fixed128
         var formatSpan = format.AsSpan();
         var precisionSpan = formatSpan.Length > 1 ? formatSpan[1..] : [];
 
-        var precision = 0;
+        var precision = -1;
         if (precisionSpan.Length != 0)
         {
-            if (!int.TryParse(precisionSpan, CultureInfo.InvariantCulture, out var requestedPrecision))
+            if (!uint.TryParse(precisionSpan, CultureInfo.InvariantCulture, out var requestedPrecision))
             {
                 throw new FormatException($"The requested format is not supported: {formatSpan}");
             }
 
-            precision = requestedPrecision;
+            precision = (int)requestedPrecision;
         }
 
-        var result = precision <= FractionalBitCount ? stackalloc char[64] : new char[64 + precision];
+        // The heap array size is a bit excessive here, but realistically, the heap allocation path is never taken
+        var result = precision <= FractionalBitCount ? stackalloc char[64] : new char[64 + M.Abs(precision)];
         var isSuccess = TryFormat(result, out var written, format, formatProvider);
         AssertUtility.IsTrue(isSuccess, "Internal: Failed to format fixed point number as a string");
 
@@ -33,6 +34,53 @@ public partial struct Fixed128
 
     public bool TryFormat(Span<char> destination, out int charsWritten, ReadOnlySpan<char> format, IFormatProvider? provider)
     {
-        return ((decimal)Raw / OneRaw).TryFormat(destination, out charsWritten, format, provider);
+        var formatType = 'G';
+        var precision = -1;
+        if (format.Length > 1)
+        {
+            formatType = char.ToUpper(format[0]);
+
+            var precisionSpan = format[1..];
+            if (precisionSpan.Length != 0)
+            {
+                if (!int.TryParse(precisionSpan, CultureInfo.InvariantCulture, out var requestedPrecision))
+                {
+                    charsWritten = 0;
+                    return false;
+                }
+
+                precision = requestedPrecision;
+            }
+        }
+
+        switch (formatType)
+        {
+            case 'G':
+            {
+                return TryFormatInternal(destination, out charsWritten, 'G', precision, provider);
+            }
+            case 'F':
+            {
+                return TryFormatInternal(destination, out charsWritten, 'F', precision, provider);
+            }
+            case 'N':
+            {
+                return TryFormatInternal(destination, out charsWritten, 'N', precision, provider);
+            }
+            case 'R':
+            {
+                return TryFormatInternal(destination, out charsWritten, 'F', -1, provider);
+            }
+            default:
+            {
+                charsWritten = 0;
+                return false;
+            }
+        }
+    }
+
+    private bool TryFormatInternal(Span<char> destination, out int charsWritten, char format, int precision, IFormatProvider? provider)
+    {
+
     }
 }
