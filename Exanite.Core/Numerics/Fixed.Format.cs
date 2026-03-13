ae96e64f6f00797421e1550571a9ad3ddce85a7e
William Chen <exanite78012@gmail.com>
2026-03-13 18:59:55 -0400

    Change Fixed to redirect to Fixed128 ToString

--- Exanite.Core/Numerics/Fixed.Format.cs
+++ Exanite.Core/Numerics/Fixed.Format.cs
@@ -6,12 +6,12 @@ public partial struct Fixed
 {
     public string ToString(string? format, IFormatProvider? formatProvider)
     {
-        return ((decimal)Raw / OneRaw).ToString(format, formatProvider);
+        return ((Fixed128)this).ToString(format, formatProvider);
     }
 
     public bool TryFormat(Span<char> destination, out int charsWritten, ReadOnlySpan<char> format, IFormatProvider? provider)
     {
-        return ((decimal)Raw / OneRaw).TryFormat(destination, out charsWritten, format, provider);
+        return ((Fixed128)this).TryFormat(destination, out charsWritten, format, provider);
     }
 
     public override string ToString() => ToString(null, null);
