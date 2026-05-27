38ab4ff3077043b1810fb0122866bd34431adcca
William Chen <exanite78012@gmail.com>
2026-05-27 17:46:35 -0400

    Use ulong as backing data type and add Count property

--- Exanite.Core.Tests/Collections/BitSetTests.cs
+++ Exanite.Core.Tests/Collections/BitSetTests.cs
@@ -16,6 +16,7 @@ public class BitSetTests
         Assert.True(bitset[0]);
         Assert.True(bitset[10]);
         Assert.True(bitset[1000]);
+        Assert.Equal(3, bitset.Count);
     }
 
     [Fact]
@@ -24,25 +25,57 @@ public class BitSetTests
         var bitset = new BitSet();
 
         Assert.True(bitset.IsEmpty);
+        Assert.Equal(0, bitset.Count);
 
         bitset[0] = true;
         Assert.False(bitset.IsEmpty);
+        Assert.Equal(1, bitset.Count);
 
         bitset[0] = false;
         Assert.True(bitset.IsEmpty);
+        Assert.Equal(0, bitset.Count);
     }
 
     [Fact]
-    public void Clear()
+    public void Clear_SetsAllBitsToFalse()
     {
         var bitset = new BitSet();
 
         Assert.True(bitset.IsEmpty);
+        Assert.Equal(0, bitset.Count);
 
         bitset[0] = true;
         Assert.False(bitset.IsEmpty);
+        Assert.Equal(1, bitset.Count);
 
         bitset.Clear();
         Assert.True(bitset.IsEmpty);
+        Assert.Equal(0, bitset.Count);
+    }
+
+    [Fact]
+    public void Clear_DoesNotResizeBackingMemory()
+    {
+        var bitset = new BitSet();
+        bitset[10000] = true;
+
+        var chunkCount = bitset.Chunks.Length;
+        bitset.Clear();
+        Assert.Equal(chunkCount, bitset.Chunks.Length);
+    }
+
+    [Fact]
+    public void Count_ReturnsNumberOfTrueBits()
+    {
+        var bitset = new BitSet();
+        for (var i = 0; i < 10000; i++)
+        {
+            bitset[i * 2] = true;
+            Assert.Equal(i + 1, bitset.Count);
+        }
+
+        bitset.Clear();
+        Assert.True(bitset.IsEmpty);
+        Assert.Equal(0, bitset.Count);
     }
 }
--- Exanite.Core/Collections/BitSet.cs
+++ Exanite.Core/Collections/BitSet.cs
@@ -1,58 +1,98 @@
 using System;
+using System.Collections;
+using System.Numerics;
 using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics;
 
 namespace Exanite.Core.Collections;
 
+/// <summary>
+/// Represents a set of bits that are set to true.
+/// Supports set operations and bitwise operations.
+/// Uses an ulong[] as the backing memory.
+/// </summary>
+/// <remarks>
+/// Compared to <see cref="BitArray"/>, this data structure does not assume an upper bound for the possible bit index.
+/// This means that this data structure does not support operations such as taking the complement of the bit set.
+/// <para/>
+/// Shift operations are also not supported.
+/// This is because this data structure is designed for storing flags, which do not make sense to bitshift.
+/// </remarks>
 public class BitSet
 {
-    private const int DefaultChunkCount = 4;
+    private const int DefaultChunkCount = 1;
     private const int GrowFactor = 2;
-    private const int BitsPerChunk = sizeof(int) * 8;
-    private const int Shift = 5;
-    private const int Mask = (1 << 5) - 1;
+    private const int BitsPerChunk = sizeof(ulong) * 8;
+    private const int Shift = 6;
+    private const int Mask = (1 << Shift) - 1;
 
-    private int[] chunks = new int[DefaultChunkCount];
+    private ulong[] chunks = new ulong[DefaultChunkCount];
 
     /// <summary>
     /// The underlying memory used to store the bits.
     /// </summary>
-    public ReadOnlySpan<int> Chunks => chunks;
+    public ReadOnlySpan<ulong> Chunks => chunks;
+
+    /// <summary>
+    /// Returns the number of true bits.
+    /// </summary>
+    /// <remarks>
+    /// This processes every bit stored in the set.
+    /// If you only need to know whether the set is empty, use <see cref="IsEmpty"/> instead.
+    /// </remarks>
+    public int Count
+    {
+        get
+        {
+            var count = 0;
+
+            var span = Chunks;
+            foreach (var chunk in span)
+            {
+                // There's no pop count equivalent for SIMD apparently
+                count += BitOperations.PopCount(chunk);
+            }
+
+            return count;
+        }
+    }
 
     /// <summary>
     /// Returns whether the bitset contains all false bits.
     /// </summary>
+    /// <remarks>
+    /// This short circuits on the first true bit, so this is faster than <see cref="Count"/>
+    /// </remarks>
     public bool IsEmpty
     {
         get
         {
             var span = Chunks;
-
-            if (Vector256.IsHardwareAccelerated && span.Length >= Vector256<int>.Count)
+            if (Vector256.IsHardwareAccelerated && span.Length >= Vector256<ulong>.Count)
             {
-                var vectorSpan = MemoryMarshal.Cast<int, Vector256<int>>(span);
+                var vectorSpan = MemoryMarshal.Cast<ulong, Vector256<ulong>>(span);
                 foreach (var vector in vectorSpan)
                 {
-                    if (vector != Vector256<int>.Zero)
+                    if (vector != Vector256<ulong>.Zero)
                     {
                         return false;
                     }
                 }
 
-                span = span[(vectorSpan.Length * Vector256<int>.Count)..];
+                span = span[(vectorSpan.Length * Vector256<ulong>.Count)..];
             }
-            else if (Vector128.IsHardwareAccelerated && span.Length >= Vector128<int>.Count)
+            else if (Vector128.IsHardwareAccelerated && span.Length >= Vector128<ulong>.Count)
             {
-                var vectorSpan = MemoryMarshal.Cast<int, Vector128<int>>(span);
+                var vectorSpan = MemoryMarshal.Cast<ulong, Vector128<ulong>>(span);
                 foreach (var vector in vectorSpan)
                 {
-                    if (vector != Vector128<int>.Zero)
+                    if (vector != Vector128<ulong>.Zero)
                     {
                         return false;
                     }
                 }
 
-                span = span[(vectorSpan.Length * Vector128<int>.Count)..];
+                span = span[(vectorSpan.Length * Vector128<ulong>.Count)..];
             }
 
             foreach (var chunk in span)
@@ -81,21 +121,21 @@ public class BitSet
             }
 
             var bit = bitIndex & Mask;
-            return (chunks[chunkIndex] & (1 << bit)) != 0;
+            return (chunks[chunkIndex] & (1UL << bit)) != 0;
         }
         set
         {
             var chunkIndex = bitIndex >> Shift;
-            EnsureCapacity(chunkIndex);
+            EnsureCapacity(chunkIndex + 1);
 
             var bit = bitIndex & Mask;
             if (value)
             {
-                chunks[chunkIndex] |= 1 << bit;
+                chunks[chunkIndex] |= 1UL << bit;
             }
             else
             {
-                chunks[chunkIndex] &= ~(1 << bit);
+                chunks[chunkIndex] &= ~(1UL << bit);
             }
         }
     }
@@ -122,7 +162,7 @@ public class BitSet
             newChunkCount *= GrowFactor;
         }
 
-        var newChunks = new int[newChunkCount];
+        var newChunks = new ulong[newChunkCount];
         chunks.CopyTo(newChunks);
         chunks = newChunks;
     }
