2a4fe0828a0f7b8e1bd613c5617e1eeaaa9c6938
William Chen <exanite78012@gmail.com>
2026-07-19 14:40:06 -0400

    Begin trying a different approach

--- Exanite.Core/Io/Globbing/PatternGlobSegment.cs
+++ Exanite.Core/Io/Globbing/PatternGlobSegment.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using Exanite.Core.Utilities;
+using Exanite.Core.Collections;
 
 namespace Exanite.Core.Io.Globbing;
 
@@ -13,114 +13,117 @@ public sealed class PatternGlobSegment : GlobSegment
 {
     public readonly string Pattern;
 
+    private readonly List<Node> Nodes = new();
+
+    /// <remarks>
+    /// This expects a pre-optimized pattern.
+    /// See <see cref="GlobPattern"/>'s parsing code.
+    /// </remarks>
     internal PatternGlobSegment(string pattern)
     {
         Pattern = pattern;
+
+        var isEscape = false;
+        for (var i = 0; i < pattern.Length; i++)
+        {
+            if (isEscape)
+            {
+                Nodes.Add(new Node(pattern[i], isEscape));
+                isEscape = false;
+                continue;
+            }
+
+            if (Pattern[i] == '\\')
+            {
+                isEscape = true;
+            }
+
+            Nodes.Add(new Node(pattern[i], isEscape));
+        }
     }
 
     public bool IsMatch(string name)
     {
-        var activeStatesList = new List<ActiveState>()
-        {
-            new(-1, default, false),
-        };
+        var maxStateCount = Nodes.Count + 1;
+        var currentStates = new BitSet();
+        var nextStates = new BitSet();
 
-        for (var nameI = 0; nameI < name.Length; nameI++)
-        {
-            var c = name[nameI];
-            var isLast = nameI == name.Length - 1;
-
-            var activeStates = activeStatesList.AsSpan();
-            for (var stateI = 0; stateI < activeStates.Length; stateI++)
-            {
-                ref var state = ref activeStates[stateI];
-
-                // Initialize state if necessary
-                if (state.PatternIndex == -1 && !TryAdvanceState(ref state))
-                {
-                    return isLast;
-                }
-
-                if (!IsMatch(ref state, c))
-                {
-                    state.IsAlive = false;
-                    continue;
-                }
-
-                // TODO: I need to figure out a better way to check free moves. This is wrong.
-                if (!state.IsEscaped && state.Operator == '*')
-                {
-                    activeStatesList.Add(state);
-                }
-
-                if (!TryAdvanceState(ref state))
-                {
-                    return isLast;
-                }
-            }
-
-            activeStatesList.RemoveAll(static state => !state.IsAlive);
-        }
-
-        // Check for free moves
-        foreach (var activeState in activeStatesList)
-        {
-            if (activeState.PatternIndex == Pattern.Length - 1 && activeState is { IsEscaped: false, Operator: '*' })
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private bool IsMatch(ref ActiveState state, char c)
-    {
-        if (!state.IsEscaped)
-        {
-            if (state.Operator == '?')
-            {
-                return true;
-            }
-
-            if (state.Operator == '*')
-            {
-                return true;
-            }
-        }
-
-        if (state.Operator == c)
-        {
-            return true;
-        }
+        // var activeStatesList = new List<ActiveState>()
+        // {
+        //     new(-1, default, false),
+        // };
+        //
+        // for (var nameI = 0; nameI < name.Length; nameI++)
+        // {
+        //     var c = name[nameI];
+        //     var isLast = nameI == name.Length - 1;
+        //
+        //     var activeStates = activeStatesList.AsSpan();
+        //     for (var stateI = 0; stateI < activeStates.Length; stateI++)
+        //     {
+        //         ref var state = ref activeStates[stateI];
+        //
+        //         // Initialize state if necessary
+        //         if (state.PatternIndex == -1 && !TryAdvanceState(ref state))
+        //         {
+        //             return isLast;
+        //         }
+        //
+        //         if (!IsMatch(ref state, c))
+        //         {
+        //             state.IsAlive = false;
+        //             continue;
+        //         }
+        //
+        //         // TODO: I need to figure out a better way to check free moves. This is wrong.
+        //         if (!state.IsEscaped && state.Operator == '*')
+        //         {
+        //             activeStatesList.Add(state);
+        //         }
+        //
+        //         if (!TryAdvanceState(ref state))
+        //         {
+        //             return isLast;
+        //         }
+        //     }
+        //
+        //     activeStatesList.RemoveAll(static state => !state.IsAlive);
+        // }
+        //
+        // // Check for free moves
+        // foreach (var activeState in activeStatesList)
+        // {
+        //     if (activeState.PatternIndex == Pattern.Length - 1 && activeState is { IsEscaped: false, Operator: '*' })
+        //     {
+        //         return true;
+        //     }
+        // }
 
         return false;
     }
 
-    private bool TryAdvanceState(ref ActiveState state)
-    {
-        var next = state.PatternIndex + 1;
-        if (next >= Pattern.Length)
-        {
-            return false;
-        }
+    // private bool IsMatch(ref ActiveState state, char c)
+    // {
+    //     if (!state.IsEscaped)
+    //     {
+    //         if (state.Operator == '?')
+    //         {
+    //             return true;
+    //         }
+    //
+    //         if (state.Operator == '*')
+    //         {
+    //             return true;
+    //         }
+    //     }
+    //
+    //     if (state.Operator == c)
+    //     {
+    //         return true;
+    //     }
+    //
+    //     return false;
+    // }
 
-        var isEscape = Pattern[next] == '\\';
-        if (!isEscape)
-        {
-            state = new ActiveState(next, Pattern[next], isEscape);
-            return true;
-        }
-
-        next++;
-        if (next >= Pattern.Length)
-        {
-            return false;
-        }
-
-        state = new ActiveState(next, Pattern[next], isEscape);
-        return true;
-    }
-
-    private record struct ActiveState(int PatternIndex, char Operator, bool IsEscaped, bool IsAlive = true);
+    private record struct Node(char Operator, bool IsEscaped);
 }
