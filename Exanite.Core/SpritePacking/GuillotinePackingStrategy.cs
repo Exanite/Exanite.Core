5d7674725c137fabb865bf64024223e39f3164a5
William Chen <exanite78012@gmail.com>
2026-08-02 05:15:29 -0400

    Add start of GuillotinePackingStrategy

---
+++ Exanite.Core/SpritePacking/GuillotinePackingStrategy.cs
@@ -0,0 +1,11 @@
+using Exanite.Core.Numerics;
+
+namespace Exanite.Core.SpritePacking;
+
+public class GuillotinePackingStrategy : ISpritePackingStrategy
+{
+    public bool TryAdd(Vector2Int size, out Rect2Int rect)
+    {
+        throw new System.NotImplementedException();
+    }
+}
